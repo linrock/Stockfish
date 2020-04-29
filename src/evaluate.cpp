dfa1b287c244f74a834e6436dc470ebce6c4aa2b
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-29 15:44:34 -0400

    MP (6,9),(8,11) king protector (20,0),(16,3) behind pawn
    Bench 4675938

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -131,10 +131,12 @@ namespace {
   constexpr Score CorneredBishop      = S( 50, 50);
   constexpr Score FlankAttacks        = S(  8,  0);
   constexpr Score Hanging             = S( 69, 36);
-  constexpr Score KingProtector       = S(  7,  8);
+  constexpr Score BishopKingProtector = S(  6,  9);
+  constexpr Score KnightKingProtector = S(  8, 11);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
-  constexpr Score MinorBehindPawn     = S( 18,  3);
+  constexpr Score BishopBehindPawn    = S( 20,  0);
+  constexpr Score KnightBehindPawn    = S( 16,  3);
   constexpr Score Outpost             = S( 30, 21);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
@@ -300,10 +302,11 @@ namespace {
 
             // Bonus for a knight or bishop shielded by pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
-                score += MinorBehindPawn;
+                score += (Pt == KNIGHT) ? KnightBehindPawn : BishopBehindPawn;
 
             // Penalty if the piece is far from the king
-            score -= KingProtector * distance(pos.square<KING>(Us), s);
+            score -= (Pt == KNIGHT ? KnightKingProtector
+                                   : BishopKingProtector) * distance(pos.square<KING>(Us), s);
 
             if (Pt == BISHOP)
             {
