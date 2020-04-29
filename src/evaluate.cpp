d446e7c814540a1602839af1edc41ebf8d15d900
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-29 17:13:39 -0400

    Bonus for knight outposts near king. Base: pro's ps_outposts25
    Bench 4822232

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -293,7 +293,15 @@ namespace {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
             if (bb & s)
+            {
                 score += Outpost * (Pt == KNIGHT ? 2 : 1);
+                if (Pt == KNIGHT)
+                {
+                    int d = 20 - 5 * distance(s, pos.square<KING>(Them));
+                    if (d > 0)
+                        score += make_score(d, 0);
+                }
+            }
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
                 score += Outpost;
