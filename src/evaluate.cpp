660bab8315588b269a9066c54d539736b7984426
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-30 12:25:58 -0400

    Lower multiplier 1.25x for outpost squares defended by 2 pawns
    Bench 4371276

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -294,14 +294,14 @@ namespace {
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
             if (bb & s) {
                 if (pawn_double_attacks_bb<Us>(pos.pieces(Us, PAWN)) & s)
-                    score += Outpost * (Pt == KNIGHT ? 2 : 1) * 3/2;
+                    score += Outpost * (Pt == KNIGHT ? 2 : 1) * 5/4;
                 else
                     score += Outpost * (Pt == KNIGHT ? 2 : 1);
             }
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us)) {
                 if (pawn_double_attacks_bb<Us>(pos.pieces(Us, PAWN)) & s)
-                    score += Outpost * 3/2;
+                    score += Outpost * 5/4;
                 else
                     score += Outpost;
             }
