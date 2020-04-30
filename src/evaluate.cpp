972e8b9b1370dbd849e1e9671a5f29a733797c28
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-30 12:02:14 -0400

    outposts3 + reduced outpost (26,15), 5/4x reachable multiplier
    Bench 4879494

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,7 +135,7 @@ namespace {
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
-  constexpr Score Outpost             = S( 30, 21);
+  constexpr Score Outpost             = S( 26, 15);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
   constexpr Score RestrictedPiece     = S(  7,  7);
@@ -301,7 +301,7 @@ namespace {
 
             else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us)) {
                 if (pawn_double_attacks_bb<Us>(pos.pieces(Us, PAWN)) & s)
-                    score += Outpost * 3/2;
+                    score += Outpost * 5/4;
                 else
                     score += Outpost;
             }
