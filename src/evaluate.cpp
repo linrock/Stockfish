e2b00808e79038d46f4c486c462ce56b8198477a
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-30 13:15:29 -0400

    MP (6,9), (16,21)/2 king protectors, (20,1), (16,3) behind pawn
    Bench 4318734

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,7 +135,8 @@ namespace {
   constexpr Score KnightKingProtector = S( 16, 21);
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
-  constexpr Score MinorBehindPawn     = S( 18,  3);
+  constexpr Score BishopBehindPawn    = S( 20,  1);
+  constexpr Score KnightBehindPawn    = S( 16,  3);
   constexpr Score Outpost             = S( 30, 21);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
@@ -301,7 +302,7 @@ namespace {
 
             // Bonus for a knight or bishop shielded by pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
-                score += MinorBehindPawn;
+                score += (Pt == BISHOP) ? BishopBehindPawn : KnightBehindPawn;
 
             // Penalty if the piece is far from the king
             if (Pt == KNIGHT)
