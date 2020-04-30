e6ef28fb6ebbeabee546bcd8d4837acc697a036d
Linmiao Xu <linmiao.xu@gmail.com>
2020-04-30 14:35:59 -0400

    More outpost bonus on central files, less bonus on flank files
    Bench 4682192

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -135,7 +135,8 @@ namespace {
   constexpr Score KnightOnQueen       = S( 16, 11);
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
-  constexpr Score Outpost             = S( 30, 21);
+  constexpr Score CentralOutpost      = S( 32, 23);
+  constexpr Score Outpost             = S( 28, 19);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
   constexpr Score RestrictedPiece     = S(  7,  7);
@@ -292,11 +293,19 @@ namespace {
         {
             // Bonus if piece is on an outpost square or can reach one
             bb = OutpostRanks & attackedBy[Us][PAWN] & ~pe->pawn_attacks_span(Them);
-            if (bb & s)
-                score += Outpost * (Pt == KNIGHT ? 2 : 1);
+            if (bb & s) {
+                if (CenterFiles & s)
+                    score += CentralOutpost * (Pt == KNIGHT ? 2 : 1);
+                else
+                    score += Outpost * (Pt == KNIGHT ? 2 : 1);
+            }
 
-            else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us))
-                score += Outpost;
+            else if (Pt == KNIGHT && bb & b & ~pos.pieces(Us)) {
+                if (CenterFiles & s)
+                    score += CentralOutpost;
+                else
+                    score += Outpost;
+            }
 
             // Bonus for a knight or bishop shielded by pawn
             if (shift<Down>(pos.pieces(PAWN)) & s)
