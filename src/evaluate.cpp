5e1cb6973175687b06c2aff0faab750175ab8bbf
Linmiao Xu <linmiao.xu@gmail.com>
2020-05-02 14:18:39 -0400

    EG bishop outposts +4
    Bench 4457116

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -137,7 +137,7 @@ namespace {
   constexpr Score LongDiagonalBishop  = S( 45,  0);
   constexpr Score MinorBehindPawn     = S( 18,  3);
   constexpr Score KnightOutpost       = S( 56, 36);
-  constexpr Score BishopOutpost       = S( 30, 23);
+  constexpr Score BishopOutpost       = S( 30, 27);
   constexpr Score ReachableOutpost    = S( 31, 22);
   constexpr Score PassedFile          = S( 11,  8);
   constexpr Score PawnlessFlank       = S( 17, 95);
