3263e3bb60bc6321b34fce4ba70a457db3cced24
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-10 00:06:04 +0000

    Add script to filtering data pieces and empty data dirs

---
+++ src/filter_data_pieces.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for i in {200..999}; do
+input_binpack="data-pieces/master_dataset_$i.binpack"
+output_binpack="data-pieces-skip789/master_dataset_$i.binpack"
+options="
+uci
+setoption name PruneAtShallowDepth value false
+setoption name Use NNUE value true
+setoption name Threads value 95
+setoption name Hash value 950000
+isready
+transform rescore depth 100 keep_moves 0 input_file ${input_binpack} output_file ${output_binpack}
+quit"
+if [ -f $output_binpack ]; then
+  echo $output_binpack already exists. Skipping
+else
+  printf "$options" | taskset -c 0-94 ./stockfish | \
+    tee data-pieces-wrong-nnue/stockfish-out-$i.txt
+fi
+done
+
+# stockfish transform rescore depth 9 input_file ${input_binpack} output_file ${output_binpack}
+
+# ls -lth $output_binpack
+# stockfish convert $output_binpack one-position.rescored.plain
