579ec90d5d1aceeec3ce010e703d42935c1a7352
Linmiao Xu <linmiao.xu@gmail.com>
2022-12-12 07:46:14 +0000

    Adjust script to iterate over data pieces again

--- src/filter_data_pieces.sh
+++ src/filter_data_pieces.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# for i in {300..999}; do
-rm -f /tmp/master_dataset_900.binpack
-input_binpack="data-pieces/master_dataset_900.binpack"
-output_binpack="/tmp/master_dataset_900.binpack"
+for i in {999..0}; do
+input_binpack="data-pieces/master_dataset_$i.binpack"
+output_binpack="data-pieces-multipv-cap-filter/master_dataset_$i.binpack"
 options="
 uci
 setoption name PruneAtShallowDepth value false
 setoption name Use NNUE value true
 setoption name Threads value 95
-setoption name Hash value 50000
+setoption name Hash value 950000
 isready
 transform rescore depth 100 keep_moves 0 input_file ${input_binpack} output_file ${output_binpack}
 quit"
 if [ -f $output_binpack ]; then
   echo $output_binpack already exists. Skipping
 else
-  printf "$options" | taskset -c 0-94 ./stockfish | \
-    tee data-pieces-wrong-nnue/stockfish-out-$i.txt
+  printf "$options" | taskset -c 0-94 ./stockfish
 fi
-# done
+done
 
 # stockfish transform rescore depth 9 input_file ${input_binpack} output_file ${output_binpack}
 
