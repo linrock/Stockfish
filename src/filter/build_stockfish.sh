9604516005c8db4c195609c5e053850e56be515e
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-01 23:21:15 +0000

    Use subdir for convenience

--- src/filter/build_stockfish.sh
+++ src/filter/build_stockfish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd ..
 make -j profile-build ARCH=x86-64-bmi2
-mv stockfish stockfish-filter-multipv2-eval-diff
+mv stockfish ./filter/stockfish-filter-multipv2-eval-diff
 echo Modified stockfish at: stockfish-filter-multipv2-eval-diff
