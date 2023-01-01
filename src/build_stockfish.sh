b5dd2937e869c2b4ba5ff3fd4723c48daad08ba7
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-01 21:45:43 +0000

    Explicitly name modified stockfish binary differently

--- src/build_stockfish.sh
+++ src/build_stockfish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 make -j profile-build ARCH=x86-64-bmi2
+mv stockfish stockfish-filter-multipv2-eval-diff
--- src/filter_data_pieces.sh
+++ src/filter_data_pieces.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-for i in {999..0}; do
-input_binpack="data-pieces/master_dataset_$i.binpack"
-output_binpack="data-pieces-multipv-cap-filter/master_dataset_$i.binpack"
+for input_binpack in $(ls -1r ./test80-nov2022-12tb7p/*.binpack); do
+output_binpack=./test80-nov2022-12tb7p-d6pv2-filtered/$(basename $input_binpack).filtered.binpack
+echo $input_binpack
+echo $output_binpack
 options="
 uci
 setoption name PruneAtShallowDepth value false
 setoption name Use NNUE value true
 setoption name Threads value 95
-setoption name Hash value 950000
+setoption name Hash value 100000
 isready
 transform rescore depth 100 keep_moves 0 input_file ${input_binpack} output_file ${output_binpack}
 quit"
+
 if [ -f $output_binpack ]; then
   echo $output_binpack already exists. Skipping
 else
-  printf "$options" | taskset -c 0-94 ./stockfish
+  printf "$options" | taskset -c 0-94 ./stockfish-filter-multipv2-eval-diff
 fi
 done
-
-# stockfish transform rescore depth 9 input_file ${input_binpack} output_file ${output_binpack}
-
-# ls -lth $output_binpack
-# stockfish convert $output_binpack one-position.rescored.plain
