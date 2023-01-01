cbf645ef603b8366d5e7ff3d7c55b1087925b2d4
Linmiao Xu <linmiao.xu@gmail.com>
2023-01-01 23:24:55 +0000

    Replace tabs with spaces

--- src/tools/transform.cpp
+++ src/tools/transform.cpp
@@ -466,7 +466,7 @@ namespace Stockfish::Tools
                         if (debug_print) {
                             sync_cout << "[debug] " << pos.fen() << sync_endl
                                       << "[debug] Position is in check" << sync_endl
-				      << "[debug]" << sync_endl;
+                                      << "[debug]" << sync_endl;
                         }
                         num_capture_or_promo_skipped.fetch_add(1);
                         num_position_in_check.fetch_add(1);
@@ -477,9 +477,9 @@ namespace Stockfish::Tools
                         if (debug_print) {
                             sync_cout << "[debug] " << pos.fen() << sync_endl
                                       << "[debug] Provided move is capture: "
-				      << UCI::move((Stockfish::Move)ps.move, false)
-				      << sync_endl
-				      << "[debug]" << sync_endl;
+                                      << UCI::move((Stockfish::Move)ps.move, false)
+                                      << sync_endl
+                                      << "[debug]" << sync_endl;
                         }
                         num_capture_or_promo_skipped.fetch_add(1);
                         num_move_already_is_capture.fetch_add(1);
@@ -505,11 +505,11 @@ namespace Stockfish::Tools
                     if (debug_print) {
                         sync_cout << "[debug] " << pos.fen() << sync_endl;
                         sync_cout << "[debug] Main PV move:    "
-				  << UCI::move(th.rootMoves[0].pv[0], false) << " "
+                                  << UCI::move(th.rootMoves[0].pv[0], false) << " "
                                   << th.rootMoves[0].score << " " << sync_endl;
                         if (th.rootMoves.size() > 1) {
                             sync_cout << "[debug] 2nd PV move:     "
-				      << UCI::move(th.rootMoves[1].pv[0], false) << " "
+                                      << UCI::move(th.rootMoves[1].pv[0], false) << " "
                                       << th.rootMoves[1].score << " " << sync_endl;
                         } else {
                             sync_cout << "[debug] The only valid move" << sync_endl;
@@ -523,47 +523,48 @@ namespace Stockfish::Tools
                         num_processed.fetch_add(1);
                         if (debug_print) {
                             sync_cout << "[debug] Move is capture: " << UCI::move(best_move, false)
-				      << sync_endl
+                                      << sync_endl
                                       << "[debug] 1st best move at depth 7 multipv 2" << sync_endl
-				      << "[debug]" << sync_endl;
+                                      << "[debug]" << sync_endl;
                         }
                         continue;
-                    } else if (th.rootMoves.size() > 1 && pos.capture_or_promotion(th.rootMoves[1].pv[0])) {
-                        // skip if multipv 2nd line bestmove is a capture or promo
-                        num_capture_or_promo_skipped.fetch_add(1);
+                    } else if (th.rootMoves.size() > 1) {
+                        if (pos.capture_or_promotion(th.rootMoves[1].pv[0])) {
+                            // skip if multipv 2nd line bestmove is a capture or promo
+                            num_capture_or_promo_skipped.fetch_add(1);
                         num_capture_or_promo_skipped_d7_multipv1.fetch_add(1);
                         num_processed.fetch_add(1);
                         if (debug_print) {
                             sync_cout << "[debug] Move is capture: " << UCI::move(best_move, false)
-				      << sync_endl
+                                      << sync_endl
                                       << "[debug] 2nd best move at depth 7 multipv 2" << sync_endl
-				      << "[debug]" << sync_endl;
+                                      << "[debug]" << sync_endl;
                         }
                         continue;
                     } else if (th.rootMoves.size() > 1) {
-		      // remove positions with only 1 good move
-		      Value m1_score = th.rootMoves[0].score;
-		      Value m2_score = th.rootMoves[1].score;
-		      if (abs(m1_score) < 100 && abs(m2_score) > 300) {
+                      // remove positions with only 1 good move
+                      Value m1_score = th.rootMoves[0].score;
+                      Value m2_score = th.rootMoves[1].score;
+                      if (abs(m1_score) < 100 && abs(m2_score) > 300) {
                         sync_cout << "[debug] best move is about equal, 2nd best move is losing" << sync_endl
-				  << "[debug]" << sync_endl;
+                                  << "[debug]" << sync_endl;
                         num_capture_or_promo_skipped_d7_multipv_eval_diff.fetch_add(1);
                         num_processed.fetch_add(1);
                         continue;
-		      } else if (abs(m1_score) > 300 && abs(m2_score) < 100) {
+                      } else if (abs(m1_score) > 300 && abs(m2_score) < 100) {
                         sync_cout << "[debug] best move gains advantage, 2nd best move equalizes" << sync_endl
-				  << "[debug]" << sync_endl;
+                                  << "[debug]" << sync_endl;
                         num_capture_or_promo_skipped_d7_multipv_eval_diff.fetch_add(1);
                         num_processed.fetch_add(1);
                         continue;
-		      } else if (abs(m1_score) > 300 &&
-				 (abs(m2_score) > 300 && ((m1_score > 0) != (m2_score > 0)))) {
+                      } else if (abs(m1_score) > 300 &&
+                                 (abs(m2_score) > 300 && ((m1_score > 0) != (m2_score > 0)))) {
                         sync_cout << "[debug] best move gains an advantage, 2nd best move loses " << sync_endl
-				  << "[debug]" << sync_endl;
+                                  << "[debug]" << sync_endl;
                         num_capture_or_promo_skipped_d7_multipv_eval_diff.fetch_add(1);
                         num_processed.fetch_add(1);
                         continue;
-		      }
+                      }
                     }
 
                     // only write the position if:
