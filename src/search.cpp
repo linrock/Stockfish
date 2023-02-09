f78969cdae15244a8add94a88a8a36fea3b7ea0b
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-09 11:59:14 -0500

    Tune 3 futility pruning numbers
    bench 3841998

--- src/search.cpp
+++ src/search.cpp
@@ -58,6 +58,13 @@ using namespace Search;
 
 namespace {
 
+  int tFutPruneChildDepth = 8;
+  int tFutPruneDenom = 304;
+  int tFutPruneEvalThresh = 28580;
+  TUNE(SetRange(4, 12), tFutPruneChildDepth);
+  TUNE(SetRange(100, 500), tFutPruneDenom);
+  TUNE(SetRange(20000, 36000), tFutPruneEvalThresh);
+
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
 
@@ -786,10 +793,10 @@ namespace {
     // Step 8. Futility pruning: child node (~40 Elo).
     // The depth condition is important for mate finding.
     if (   !ss->ttPv
-        &&  depth < 8
-        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / 304 >= beta
+        &&  depth < tFutPruneChildDepth
+        &&  eval - futility_margin(depth, improving) - (ss-1)->statScore / tFutPruneDenom >= beta
         &&  eval >= beta
-        &&  eval < 28580) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
+        &&  eval < tFutPruneEvalThresh) // larger than VALUE_KNOWN_WIN, but smaller than TB wins
         return eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
