f12db046899a67edc5879cc77f2a0e14c29f2431
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-09 18:55:37 -0500

    Try values from 738k / 1MM at 30+0.3
    bench 4054683

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -193,26 +193,17 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  Value LazyThreshold1    =  Value(3631);
-  Value LazyThreshold2    =  Value(2084);
-  Value SpaceThreshold    =  Value(11551);
-  TUNE(SetRange(2000,4000), LazyThreshold1);
-  TUNE(SetRange(1500,2500), LazyThreshold2);
-  TUNE(SetRange(11000,12000), SpaceThreshold);
+  Value LazyThreshold1    =  Value(3553);
+  Value LazyThreshold2    =  Value(2041);
+  Value SpaceThreshold    =  Value(11707);
 
-  int useClassicalPsqtThresh = 1781;
-  TUNE(SetRange(1700, 1900), useClassicalPsqtThresh);
-  int nnueComplexityScale = 406;
-  TUNE(SetRange(300, 500), nnueComplexityScale);
-  int nnueComplexityOffset = 272;
-  TUNE(SetRange(250, 300), nnueComplexityOffset);
-  int optimismScaleOffset = 748;
-  TUNE(SetRange(650, 850), optimismScaleOffset);
+  int useClassicalPsqtThresh = 1825;
+  int nnueComplexityScale = 369;
+  int nnueComplexityOffset = 274;
+  int optimismScaleOffset = 818;
 
-  int pDamp1 = 200;
-  TUNE(SetRange(100,300), pDamp1);
-  int pDamp2 = 214;
-  TUNE(SetRange(100,300), pDamp2);
+  int pDamp1 = 208;
+  int pDamp2 = 240;
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 76, 46, 45, 14 };
