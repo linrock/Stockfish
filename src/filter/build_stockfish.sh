58b858ad641ee9a6efb65d999a2cc96283f1916f
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-10 06:50:42 +0000

    Update script for building stockfish

--- src/filter/build_stockfish.sh
+++ src/filter/build_stockfish.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ..
-make -j profile-build ARCH=x86-64-bmi2
-mv stockfish ./filter/stockfish-filter-multipv2-eval-diff
-echo Modified stockfish at: stockfish-filter-multipv2-eval-diff
+# make -j profile-build ARCH=x86-64-bmi2
+make -j build ARCH=x86-64-bmi2
+mv stockfish ./filter/stockfish-output-positions-csv
+echo Modified stockfish at: stockfish-output-positions-csv
