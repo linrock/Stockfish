ce123dc10e5ba21cee838282be9f4e7bc990063f
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-13 10:24:25 -0500

    100% bayes-tune-nnue-scale @ 60+0.6
    bench 3701863

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos, int* complexity) {
   else
   {
       int nnueComplexity;
-      int scale = 1076 + 96 * pos.non_pawn_material() / 5120;
+      int scale = 1003 + 9 * pos.count<PAWN>() + 61 * pos.non_pawn_material() / 4096;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
