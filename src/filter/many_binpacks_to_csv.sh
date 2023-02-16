8cda60ea6b9b97b3c90d20095c98c1b862b63169
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-16 07:27:15 +0000

    Take output dir as an arg when converting to csv

--- src/filter/many_binpacks_to_csv.sh
+++ src/filter/many_binpacks_to_csv.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-  echo "Usage: ./many_binpacks_to_csv.sh <data_dir>"
+if [ "$#" -ne 2 ]; then
+  echo "Usage: ./many_binpacks_to_csv.sh <input_binpack_dir> <output_csv_dir>"
   exit 0
 fi
 
 function binpack_to_csv() {
   output_filename=$(basename $1).csv
-  output_dir=./data/nnue-tools/training-data/dfrc-positions-csv/
+  output_dir=$2
   echo "Filtering... $1 -> $output_dir/$output_filename"
   ./binpack_to_csv.sh $1 | grep "d6 pv2" > $output_dir/$output_filename
 }
 export -f binpack_to_csv
 
-# Filters positions out of .plain files concurrently
+# Converts binpacks to csv with search eval data for each position
 concurrency=$(( $(nproc) - 1 ))
-ls -1v $1/*.binpack | xargs -P $concurrency -I{} bash -c 'binpack_to_csv "$@"' _ {}
+ls -1v $1/*.binpack | xargs -P $concurrency -I{} bash -c 'binpack_to_csv "$@"' _ {} $2
