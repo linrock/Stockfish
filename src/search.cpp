2cb68d8b063b0c11b865be24a1fd8091c89a1c08
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-21 15:24:51 -0500

    Tune 6 search: 3 stat score hist reduc + 3 futility pruning
    bench 4245271

--- src/search.cpp
+++ src/search.cpp
@@ -59,13 +59,18 @@ using namespace Search;
 namespace {
 
   int TUNE_histDenom = 7208;
-  int TUNE_lmrDepth = 13;
   int TUNE_staticEvalOffset = 103;
   int TUNE_lmrDepthMult = 136;
   TUNE(SetRange(6800, 7600), TUNE_histDenom);
-  TUNE(SetRange(10, 16), TUNE_lmrDepth);
   TUNE(SetRange(70, 130), TUNE_staticEvalOffset);
   TUNE(SetRange(100, 172), TUNE_lmrDepthMult);
+
+  int TUNE_statScoreOffset = 4467;
+  int TUNE_reducDenomConst = 12800;
+  int TUNE_reducDenomMidMult = 4410;
+  TUNE(SetRange(4000, 4900), TUNE_statScoreOffset);
+  TUNE(SetRange(10000, 15600), TUNE_reducDenomConst);
+  TUNE(SetRange(4000, 4820), TUNE_reducDenomMidMult);
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
@@ -1043,7 +1048,7 @@ moves_loop: // When in check, search starts here
 
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
-                  && lmrDepth < TUNE_lmrDepth
+                  && lmrDepth < 14
                   && ss->staticEval + TUNE_staticEvalOffset + TUNE_lmrDepthMult * lmrDepth <= alpha)
                   continue;
 
@@ -1188,10 +1193,10 @@ moves_loop: // When in check, search starts here
                      + (*contHist[0])[movedPiece][to_sq(move)]
                      + (*contHist[1])[movedPiece][to_sq(move)]
                      + (*contHist[3])[movedPiece][to_sq(move)]
-                     - 4467;
+                     - TUNE_statScoreOffset;
 
       // Decrease/increase reduction for moves with a good/bad history (~30 Elo)
-      r -= ss->statScore / (12800 + 4410 * (depth > 7 && depth < 19));
+      r -= ss->statScore / (TUNE_reducDenomConst + TUNE_reducDenomMidMult * (depth > 7 && depth < 19));
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
