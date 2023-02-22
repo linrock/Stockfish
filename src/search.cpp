19e85a9fd879b2bc471951080d7f13aa9c4ee90c
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-22 10:08:22 -0500

    tbpsa 6 iters, 96 evals, 23k games, 96+64 batch, 1.5M npm
    bench 4494798

--- src/search.cpp
+++ src/search.cpp
@@ -58,10 +58,10 @@ using namespace Search;
 
 namespace {
 
-  int TUNE_histDenom = 7202;
-  int TUNE_lmrDepth = 13;
-  int TUNE_staticEvalOffset = 111;
-  int TUNE_lmrDepthMult = 137;
+  constexpr int TUNE_histDenom = 7202;
+  constexpr int TUNE_lmrDepth = 13;
+  constexpr int TUNE_staticEvalOffset = 111;
+  constexpr int TUNE_lmrDepthMult = 137;
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV, Root };
