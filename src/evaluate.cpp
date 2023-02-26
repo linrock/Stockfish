2257acc3dda04bd64d9d897004eb1f05f5ddc119
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-26 17:50:28 -0500

    Tune 8 eval params, pawn mult x32
    bench 4380438

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,25 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  Value LazyThreshold1 = Value(3622);
+  Value LazyThreshold2 = Value(1962);
+  TUNE(SetRange(2800, 3800), LazyThreshold1);
+  TUNE(SetRange(1400, 2400), LazyThreshold2);
+
+  int TUNE_psqThresh = 1781;
+  int TUNE_nnueComplexityMult = 406;
+  int TUNE_nnueOptCompOffset = 272;
+  TUNE(SetRange(1750, 1810), TUNE_psqThresh);
+  TUNE(SetRange(380, 430), TUNE_nnueComplexityMult);
+  TUNE(SetRange(250, 300), TUNE_nnueOptCompOffset);
+
+  int TUNE_scaleBase = 1001;
+  int TUNE_scalePcMult = 160;
+  int TUNE_scaleNonPawnMat = 61;
+  TUNE(SetRange(900, 1100), TUNE_scaleBase);
+  TUNE(SetRange(0, 320), TUNE_scalePcMult);
+  TUNE(SetRange(40, 80), TUNE_scaleNonPawnMat);
+
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1063,7 +1080,9 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 1001 + pos.non_pawn_material() / 64;
+      int scale = TUNE_scaleBase
+                + TUNE_scalePcMult * pos.count<PAWN>() / 32
+                + TUNE_scaleNonPawnMat * pos.non_pawn_material() / 4096;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1071,11 +1090,11 @@ Value Eval::evaluate(const Position& pos) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  406 * nnueComplexity
+      nnueComplexity = (  TUNE_nnueComplexityMult * nnueComplexity
                         + (424 + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
+      optimism = optimism * (TUNE_nnueOptCompOffset + nnueComplexity) / 256;
       v = (nnue * scale + optimism * (scale - 748)) / 1024;
   }
 
