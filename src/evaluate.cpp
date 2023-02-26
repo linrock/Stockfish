e93ba4ab75d8857279ca7b3641f53f6ba57bdd3b
Linmiao Xu <linmiao.xu@gmail.com>
2023-02-26 17:50:28 -0500

    tbpsa 9 iters ng-eval-8p-1M-1024-32
    bench 3870203

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,15 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  constexpr Value LazyThreshold1 = Value(3507);
+  constexpr Value LazyThreshold2 = Value(1752);
+
+  constexpr   int TUNE_nnueComplexityMult = 403;
+  constexpr   int TUNE_nnueOptCompOffset = 275;
+  constexpr   int TUNE_scaleBase = 989;
+  constexpr   int TUNE_scalePcMult = 5;
+  constexpr   int TUNE_scaleNonPawnMat = 52;
+
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1063,7 +1070,7 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 1001 + pos.non_pawn_material() / 64;
+      int scale = TUNE_scaleBase + TUNE_scalePcMult * pos.count<PAWN>() + TUNE_scaleNonPawnMat * pos.non_pawn_material() / 4096;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1071,11 +1078,11 @@ Value Eval::evaluate(const Position& pos) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  406 * nnueComplexity
+      nnueComplexity = (  TUNE_nnueComplexityMult * nnueComplexity
                         + (424 + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
+      optimism = optimism * (TUNE_nnueOptCompOffset + nnueComplexity) / 256;
       v = (nnue * scale + optimism * (scale - 748)) / 1024;
   }
 
