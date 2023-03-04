794e3e5f1d923b75e5dbe0f47fca04d323794ba7
Linmiao Xu <linmiao.xu@gmail.com>
2023-03-04 17:11:05 -0500

    Subdir for running test cases

--- data/build_stockfish.sh
+++ data/build_stockfish.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cd ..
-# make -j profile-build ARCH=x86-64-bmi2
-make -j build ARCH=x86-64-bmi2
-mv stockfish ./filter/stockfish-output-positions-csv
+cd ../src
+make -j profile-build ARCH=x86-64-bmi2
+mv stockfish ../data/stockfish-output-positions-csv
 echo Modified stockfish at: stockfish-output-positions-csv
