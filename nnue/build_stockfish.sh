02e62ddd5faec4dc34eeec90010e223fb788585b
Linmiao Xu <linmiao.xu@gmail.com>
2023-03-04 17:33:46 -0500

    Fix moving compiled bin over

--- nnue/build_stockfish.sh
+++ nnue/build_stockfish.sh
@@ -2,5 +2,5 @@
 
 cd ../src
 make -j profile-build ARCH=x86-64-bmi2
-mv stockfish ../data/stockfish-output-positions-csv
+mv stockfish ../nnue/stockfish-output-positions-csv
 echo Modified stockfish at: stockfish-output-positions-csv
