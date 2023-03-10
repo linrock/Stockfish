369b7d5cbdb402f1c6759a9b93dabfdb4e155ab7
Linmiao Xu <linmiao.xu@gmail.com>
2023-03-10 08:57:17 -0500

    tbpsa 11 iters, 720 evals, 444k games, ltc 20+0.2
    bench 5404769

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -193,27 +193,17 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  Value LazyThreshold1 = Value(3622);
-  Value LazyThreshold2 = Value(1962);
-  TUNE(SetRange(3000, 4000), LazyThreshold1);
-  TUNE(SetRange(1400, 2400), LazyThreshold2);
+  Value LazyThreshold1 = Value(3564);
+  Value LazyThreshold2 = Value(1868);
 
-  int TUNE_psqThresh = 1781;
-  int TUNE_scaleBase = 1001;
-  int TUNE_scalePawnMult = 5;
-  int TUNE_scaleNonPawnMult = 61;
-  int TUNE_nnueComplexityMult = 406;
-  int TUNE_complexityOptOffset = 424;
-  int TUNE_nnueOptCompOffset = 272;
-  int TUNE_vScaleOffset = 748;
-  TUNE(SetRange(1730, 1830), TUNE_psqThresh);
-  TUNE(SetRange(800, 1200), TUNE_scaleBase);
-  TUNE(SetRange(0, 15), TUNE_scalePawnMult);
-  TUNE(SetRange(40, 80), TUNE_scaleNonPawnMult);
-  TUNE(SetRange(380, 430), TUNE_nnueComplexityMult);
-  TUNE(SetRange(394, 454), TUNE_complexityOptOffset);
-  TUNE(SetRange(240, 310), TUNE_nnueOptCompOffset);
-  TUNE(SetRange(700, 800), TUNE_vScaleOffset);
+  int TUNE_psqThresh = 1763;
+  int TUNE_scaleBase = 934;
+  int TUNE_scalePawnMult = 7;
+  int TUNE_scaleNonPawnMult = 67;
+  int TUNE_nnueComplexityMult = 407;
+  int TUNE_complexityOptOffset = 419;
+  int TUNE_nnueOptCompOffset = 281;
+  int TUNE_vScaleOffset = 742;
 
   constexpr Value SpaceThreshold    =  Value(11551);
 
