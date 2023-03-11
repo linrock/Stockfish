f9853c41ddaa5295d19b091bcaccb6c924199ad4
Linmiao Xu <linmiao.xu@gmail.com>
2023-03-11 00:57:38 -0500

    tbpsa 8 iters, 80 evals, 50k games, 512+32 batch, 30+0.3
    bench 4555789

--- src/search.cpp
+++ src/search.cpp
@@ -37,10 +37,8 @@
 
 namespace Stockfish {
 
-    int TUNE_nullMoveOffset = 253;
-    int TUNE_futPrunOffset = 103;
-    TUNE(SetRange(200, 300), TUNE_nullMoveOffset);
-    TUNE(SetRange(50, 150), TUNE_futPrunOffset);
+    int TUNE_nullMoveOffset = 237;
+    int TUNE_futPrunOffset = 88;
 
 namespace Search {
 
