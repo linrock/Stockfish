373c1453455a729b3c83b8a52f8eef9df622af25
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-25 23:13:36 -0400

    tune 5 optimism params in search and eval
    bench 3714847

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -60,6 +60,13 @@ namespace Stockfish {
 
 namespace Eval {
 
+  int TUNE_evalOptOffset = 424;
+  int TUNE_evalOptComplexityOffset = 272;
+  int TUNE_scaleOffset = 748;
+  TUNE(SetRange(324, 524), TUNE_evalOptOffset);
+  TUNE(SetRange(172, 372), TUNE_evalOptComplexityOffset);
+  TUNE(SetRange(648, 848), TUNE_scaleOffset);
+
   bool useNNUE;
   string currentEvalFileName = "None";
 
@@ -1072,11 +1079,11 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
+                        + (TUNE_evalOptOffset + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      optimism = optimism * (TUNE_evalOptComplexityOffset + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - TUNE_scaleOffset)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
