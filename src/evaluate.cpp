f6dbdde4f0c7f212719d1f33a588096219155d45
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-28 09:17:12 -0400

    nnue-opt-5p 45+0.45
    bench 3583145

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1072,11 +1072,11 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
+                        + (417 + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      optimism = optimism * (308 + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - 734)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
