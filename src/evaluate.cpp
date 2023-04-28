7b202e89ce0aa2cb5a83140449a2d83c9e30aaa2
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-28 01:38:20 -0400

    ngEvalLazy5p-1 no damp + nnue-opt-5p 30+0.3
    bench 4053163

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,8 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  constexpr Value LazyThreshold1    =  Value(3398);
+  constexpr Value LazyThreshold2    =  Value(1764);
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1071,12 +1071,12 @@ Value Eval::evaluate(const Position& pos) {
       Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
       // Blend nnue complexity with (semi)classical complexity
-      nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
+      nnueComplexity = (  399 * nnueComplexity
+                        + (364 + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      optimism = optimism * (254 + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - 795)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
