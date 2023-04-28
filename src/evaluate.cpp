5db27d5fe9a5a8386dd96d64a708e7faa7d3afef
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-28 14:37:04 -0400

    nnueOpt5p-1 and ng-eval-6p-14
    bench 3791275

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,8 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  constexpr Value LazyThreshold1    =  Value(3440);
+  constexpr Value LazyThreshold2    =  Value(1896);
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1063,7 +1063,7 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 1001 + pos.non_pawn_material() / 64;
+      int scale = 988 + 5 * pos.count<PAWN>() + 54 * pos.non_pawn_material() / 4096;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1072,11 +1072,11 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  406 * nnueComplexity
-                        + (424 + optimism) * abs(psq - nnue)
+                        + (364 + optimism) * abs(psq - nnue)
                         ) / 1024;
 
-      optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      optimism = optimism * (254 + nnueComplexity) / 256;
+      v = (nnue * scale + optimism * (scale - 795)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
