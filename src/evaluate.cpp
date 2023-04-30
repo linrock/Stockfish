634f1ce1a8704d873984d8b2628bf250726ef249
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-29 21:34:34 -0400

    tune nnue and optimism scales separately
    bench 3739675

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,6 +58,20 @@ using namespace std;
 
 namespace Stockfish {
 
+      int TUNE_nnueScaleBase = 967;
+      int TUNE_nnueScalePawnMult = 0;
+      int TUNE_nnueScaleNpMult = 32;
+      TUNE(SetRange(867, 1067), TUNE_nnueScaleBase);
+      TUNE(SetRange(-100, 100), TUNE_nnueScalePawnMult);
+      TUNE(SetRange(-32, 96), TUNE_nnueScaleNpMult);
+
+      int TUNE_optScaleBase = 176;
+      int TUNE_optScalePawnMult = 0;
+      int TUNE_optScaleNpMult = 32;
+      TUNE(SetRange(0, 352), TUNE_optScaleBase);
+      TUNE(SetRange(-100, 100), TUNE_optScalePawnMult);
+      TUNE(SetRange(-32, 96), TUNE_optScaleNpMult);
+
 namespace Eval {
 
   bool useNNUE;
@@ -1063,7 +1077,12 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 967 + pos.non_pawn_material() / 64;
+      int nnueScale = TUNE_nnueScaleBase
+                    + TUNE_nnueScalePawnMult * pos.count<PAWN>() / 8
+                    + TUNE_nnueScaleNpMult * pos.non_pawn_material() / 2048;
+      int optScale  = TUNE_optScaleBase
+                    + TUNE_optScalePawnMult * pos.count<PAWN>() / 8
+                    + TUNE_optScaleNpMult * pos.non_pawn_material() / 2048;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1076,7 +1095,7 @@ Value Eval::evaluate(const Position& pos) {
                         ) / 1024;
 
       optimism = optimism * (274 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 791)) / 1024;
+      v = (nnue * nnueScale + optimism * optScale) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
