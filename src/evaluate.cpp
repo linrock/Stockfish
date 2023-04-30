1ae3d2be8669df13f1e7f2feb122f9cf909afb73
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-29 21:34:34 -0400

    Try neg opt pawn mult
    bench 3742562

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1063,7 +1063,8 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 1001 + pos.non_pawn_material() / 64;
+      int nnueScale = 920 + 5 * pos.count<PAWN>() + 3 * pos.non_pawn_material() / 512;
+      int optScale  = 180 - 3 * pos.count<PAWN>() + 9 * pos.non_pawn_material() / 512;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1076,7 +1077,7 @@ Value Eval::evaluate(const Position& pos) {
                         ) / 1024;
 
       optimism = optimism * (272 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 748)) / 1024;
+      v = (nnue * nnueScale + optimism * optScale) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
