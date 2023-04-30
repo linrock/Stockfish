05fc5cc35315109f6e2bc6dee78a98eb53fba7ee
Linmiao Xu <linmiao.xu@gmail.com>
2023-04-29 21:34:34 -0400

    tune nnue and optimism scales, allow neg opt
    bench 3573070

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,6 +58,19 @@ using namespace std;
 
 namespace Stockfish {
 
+      int TUNE_nnueScaleBase = 967;
+      int TUNE_nnueScaleNpMult = 32;
+      TUNE(SetRange(867, 1067), TUNE_nnueScaleBase);
+      TUNE(SetRange(-32, 96), TUNE_nnueScaleNpMult);
+
+      int TUNE_optScaleBase = 176;
+      int TUNE_optScaleNpMult = 32;
+      TUNE(SetRange(0, 352), TUNE_optScaleBase);
+      TUNE(SetRange(-32, 96), TUNE_optScaleNpMult);
+
+      int TUNE_complexityMult = 454;
+      TUNE(SetRange(354, 554), TUNE_complexityMult);
+
 namespace Eval {
 
   bool useNNUE;
@@ -1063,7 +1076,10 @@ Value Eval::evaluate(const Position& pos) {
   else
   {
       int nnueComplexity;
-      int scale = 967 + pos.non_pawn_material() / 64;
+      int nnueScale = TUNE_nnueScaleBase
+                    + TUNE_nnueScaleNpMult * pos.non_pawn_material() / 2048;
+      int optScale  = TUNE_optScaleBase
+                    + TUNE_optScaleNpMult * pos.non_pawn_material() / 2048;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1072,11 +1088,12 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  402 * nnueComplexity
-                        + (454 + optimism) * abs(psq - nnue)
+                        + TUNE_complexityMult * abs(psq - nnue)
+                        + int(optimism) * int(psq - nnue)
                         ) / 1024;
 
       optimism = optimism * (274 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 791)) / 1024;
+      v = (nnue * nnueScale + optimism * optScale) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
