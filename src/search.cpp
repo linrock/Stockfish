c47d72aa1534eda29faab5cf924b60393427b8af
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-01 15:26:39 -0400

    385/386 tune-nnue-opt-sc-9p 25+0.25
    bench 3909823

--- src/search.cpp
+++ src/search.cpp
@@ -38,10 +38,8 @@
 
 namespace Stockfish {
 
-  int TUNE_searchOptBase = 102;
-  int TUNE_searchOptDenom = 147;
-  TUNE(SetRange(2, 202), TUNE_searchOptBase);
-  TUNE(SetRange(47, 247), TUNE_searchOptDenom);
+constexpr int TUNE_searchOptBase = 87;
+constexpr int TUNE_searchOptDenom = 150;
 
 namespace Search {
 
