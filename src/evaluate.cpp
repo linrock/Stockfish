d709a00c7acfc6332e6b6ae8799ad9f5e9b4d9a3
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-01 14:17:58 -0400

    Tune 5 nnue/classic threshold params, neg opt
    bench 3178866

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,6 +58,18 @@ using namespace std;
 
 namespace Stockfish {
 
+  int TUNE_scaleBase = 967;
+  int TUNE_nnueComplexityOptOffset = 454;
+  int TUNE_optScaleOffset = 791;
+  TUNE(SetRange(867, 1067), TUNE_scaleBase);
+  TUNE(SetRange(354, 554), TUNE_nnueComplexityOptOffset);
+  TUNE(SetRange(691, 891), TUNE_optScaleOffset);
+
+  int TUNE_nnuePieceCountThresh = 2;
+  int TUNE_nnuePsqClassicThresh = 2048;
+  TUNE(SetRange(2, 30), TUNE_nnuePieceCountThresh);
+  TUNE(SetRange(1548, 2548), TUNE_nnuePsqClassicThresh);
+
 namespace Eval {
 
   bool useNNUE;
@@ -1056,14 +1068,15 @@ Value Eval::evaluate(const Position& pos) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || abs(psq) > 2048;
+  bool useClassical = !useNNUE || (pos.count<ALL_PIECES>() > TUNE_nnuePieceCountThresh &&
+                                                  abs(psq) > TUNE_nnuePsqClassicThresh);
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
   else
   {
       int nnueComplexity;
-      int scale = 967 + pos.non_pawn_material() / 64;
+      int scale = TUNE_scaleBase + pos.non_pawn_material() / 64;
 
       Color stm = pos.side_to_move();
       Value optimism = pos.this_thread()->optimism[stm];
@@ -1072,11 +1085,12 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend nnue complexity with (semi)classical complexity
       nnueComplexity = (  402 * nnueComplexity
-                        + (454 + optimism) * abs(psq - nnue)
+                        + TUNE_nnueComplexityOptOffset * abs(psq - nnue)
+                        + int(optimism) * int(psq - nnue)
                         ) / 1024;
 
       optimism = optimism * (274 + nnueComplexity) / 256;
-      v = (nnue * scale + optimism * (scale - 791)) / 1024;
+      v = (nnue * scale + optimism * (scale - TUNE_optScaleOffset)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
