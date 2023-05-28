9234a10eef0639acd40ab09a9a48b062e6c7b570
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-28 09:35:10 -0400

    Try 7 tuned eval and opt params, lazy
    bench 2537539

--- src/search.cpp
+++ src/search.cpp
@@ -353,7 +353,7 @@ void Thread::search() {
           beta  = std::min(prev + delta, VALUE_INFINITE);
 
           // Adjust optimism based on root move's previousScore
-          int opt = 116 * prev / (std::abs(prev) + 143);
+          int opt = 124 * prev / (std::abs(prev) + 179);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 
