f1a7c01248baefc15220f9d20a672b111ab400e2
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-30 18:24:54 -0400

    Try removing eval threshold when checking extensions
    bench 2546607

--- src/search.cpp
+++ src/search.cpp
@@ -1102,9 +1102,7 @@ moves_loop: // When in check, search starts here
           }
 
           // Check extensions (~1 Elo)
-          else if (   givesCheck
-                   && depth > 9
-                   && abs(ss->staticEval) > 87)
+          else if (givesCheck && depth > 9)
               extension = 1;
 
           // Quiet ttMove extensions (~1 Elo)
