2010bf35596b42bd26c745fdbd63ef4fe5b1475c
Linmiao Xu <linmiao.xu@gmail.com>
2023-05-30 23:32:31 -0400

    Try also removing piece value
    bench 2618184

--- src/search.cpp
+++ src/search.cpp
@@ -986,7 +986,7 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 207 + 223 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 207 + 223 * lmrDepth
                    < alpha)
                   continue;
 
