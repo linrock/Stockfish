c635c9bbc5f5499bceb15977211743a54e2593c6
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-08 09:48:30 -0400

    Change eval damp constants
    bench 2644186

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1076,7 +1076,7 @@ Value Eval::evaluate(const Position& pos) {
   }
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (200 - pos.rule50_count()) / 214;
+  v = v * (234 - pos.rule50_count()) / 256;
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
