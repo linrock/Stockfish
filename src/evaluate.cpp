6de43b17e266fca3aae1ea2d48eda0edbe528f24
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-09 19:46:29 -0400

    Try some tuned values with classic damp 234
    bench 2758803

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1056,7 +1056,7 @@ Value Eval::evaluate(const Position& pos) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || abs(psq) > 2048;
+  bool useClassical = !useNNUE || abs(psq) > 2030;
 
   if (useClassical) {
       v = Evaluation<NO_TRACE>(pos).value();
@@ -1076,8 +1076,8 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend optimism with nnue complexity and (semi)classical complexity
       optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
-      v = (nnue * (945 + npm) + optimism * (150 + npm)) / 1024;
-      v = v * (200 - pos.rule50_count()) / 214;
+      v = (nnue * (1050 + npm) + optimism * (134 + npm)) / 1024;
+      v = v * (196 - pos.rule50_count()) / 214;
   }
 
   // Guarantee evaluation does not hit the tablebase range
