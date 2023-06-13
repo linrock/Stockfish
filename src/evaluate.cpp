d65f2a72035314af2a7c154a685aca4d4ac46a47
Linmiao Xu <linmiao.xu@gmail.com>
2023-06-12 20:36:17 -0400

    Try approximate tuned values
    bench 2999356

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1056,7 +1056,7 @@ Value Eval::evaluate(const Position& pos) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || abs(psq) > 2048;
+  bool useClassical = !useNNUE || abs(psq) > 2100;
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
@@ -1072,7 +1072,7 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend optimism with nnue complexity and (semi)classical complexity
       optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
-      v = (nnue * (945 + npm) + optimism * (150 + npm)) / 1024;
+      v = (nnue * (978 + npm) + optimism * (96 + npm)) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
