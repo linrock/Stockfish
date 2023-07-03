eac2a4ea13e6f943dd6430d11dc2d0d2dad21cbf
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-02 22:26:52 -0400

    Tune 7 eval params, lazy thresh
    bench 2427629

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,6 +58,22 @@ using namespace std;
 
 namespace Stockfish {
 
+  Value TUNE_LazyThreshold1 = Value(3622);
+  Value TUNE_LazyThreshold2 = Value(1962);
+  TUNE(SetRange(3322, 3922), TUNE_LazyThreshold1);
+  TUNE(SetRange(1662, 2262), TUNE_LazyThreshold2);
+
+  int TUNE_psqThresh = 2048;
+  int TUNE_nnueNpmBase = 945;
+  int TUNE_nnuePcMult = 0;
+  int TUNE_optNpmBase = 150;
+  int TUNE_optPcMult = 0;
+  TUNE(SetRange(1748, 2348), TUNE_psqThresh);
+  TUNE(SetRange(745, 1145), TUNE_nnueNpmBase);
+  TUNE(SetRange(-16, 16), TUNE_nnuePcMult);
+  TUNE(SetRange(0, 300), TUNE_optNpmBase);
+  TUNE(SetRange(-16, 16), TUNE_optPcMult);
+
 namespace Eval {
 
   bool useNNUE;
@@ -191,8 +207,8 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  Value LazyThreshold1    =  TUNE_LazyThreshold1;
+  Value LazyThreshold2    =  TUNE_LazyThreshold2;
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
@@ -1056,7 +1072,7 @@ Value Eval::evaluate(const Position& pos) {
   // We use the much less accurate but faster Classical eval when the NNUE
   // option is set to false. Otherwise we use the NNUE eval unless the
   // PSQ advantage is decisive. (~4 Elo at STC, 1 Elo at LTC)
-  bool useClassical = !useNNUE || abs(psq) > 2048;
+  bool useClassical = !useNNUE || abs(psq) > TUNE_psqThresh;
 
   if (useClassical)
       v = Evaluation<NO_TRACE>(pos).value();
@@ -1072,7 +1088,8 @@ Value Eval::evaluate(const Position& pos) {
 
       // Blend optimism with nnue complexity and (semi)classical complexity
       optimism += optimism * (nnueComplexity + abs(psq - nnue)) / 512;
-      v = (nnue * (945 + npm) + optimism * (150 + npm)) / 1024;
+      v = (nnue     * (TUNE_nnueNpmBase + npm + TUNE_nnuePcMult * pos.count<PAWN>()) +
+           optimism * (TUNE_optNpmBase + npm + TUNE_optPcMult * pos.count<PAWN>())) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
