a76aec20632de5f5cbc754acc636c394b7691f43
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-17 20:59:23 -0400

    nn-1ceb1ade2560.nnue
    bench 1539541

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -38,7 +38,7 @@ namespace Stockfish::Eval::NNUE {
 using FeatureSet = Features::HalfKAv2_hm;
 
 // Number of input feature dimensions after conversion
-constexpr IndexType TransformedFeatureDimensions = 2560;
+constexpr IndexType TransformedFeatureDimensions = 3072;
 constexpr IndexType PSQTBuckets = 8;
 constexpr IndexType LayerStacks = 8;
 
