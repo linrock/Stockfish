be68e4675cb98e5e31b3a913243f3cecab662419
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-24 01:10:40 -0400

    Tune 6 eval params
    bench 1601764

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -54,6 +54,19 @@ using namespace std;
 
 namespace Stockfish {
 
+int TUNE_npmMult = 32;
+int TUNE_nnueNpmBase = 915;
+int TUNE_optNpmBase = 154;
+int TUNE_nnueScalePc = 9;
+int TUNE_optScalePc = 1;
+int TUNE_dampConst = 200;
+TUNE(SetRange(0, 64), TUNE_npmMult);
+TUNE(SetRange(715, 1115), TUNE_nnueNpmBase);
+TUNE(SetRange(0, 308), TUNE_optNpmBase);
+TUNE(SetRange(-24, 24), TUNE_nnueScalePc);
+TUNE(SetRange(-24, 24), TUNE_optScalePc);
+TUNE(SetRange(100, 300), TUNE_dampConst);
+
 namespace Eval {
 
   string currentEvalFileName = "None";
@@ -145,7 +158,7 @@ Value Eval::evaluate(const Position& pos) {
   Value v;
 
   int nnueComplexity;
-  int npm = pos.non_pawn_material() / 64;
+  int npm = TUNE_npmMult * pos.non_pawn_material() / 2048;
 
   Color stm = pos.side_to_move();
   Value optimism = pos.this_thread()->optimism[stm];
@@ -161,11 +174,11 @@ Value Eval::evaluate(const Position& pos) {
   // Blend optimism with nnue complexity and (semi)classical complexity
   optimism += optimism * (nnueComplexity + abs(material - nnue)) / 512;
 
-  v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
-       + optimism * (154 + npm +     pos.count<PAWN>())) / 1024;
+  v = (  nnue     * (TUNE_nnueNpmBase + npm + TUNE_nnueScalePc * pos.count<PAWN>())
+       + optimism * (TUNE_optNpmBase  + npm + TUNE_optScalePc  * pos.count<PAWN>())) / 1024;
 
   // Damp down the evaluation linearly when shuffling
-  v = v * (200 - pos.rule50_count()) / 214;
+  v = v * (TUNE_dampConst - pos.rule50_count()) / (TUNE_dampConst + 14);
 
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
