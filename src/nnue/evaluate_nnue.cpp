d18705042a07fd5ce06fe0458b42f12e0ef0ddca
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-25 20:34:59 -0400

    Remove abs(positional) term
    bench 1526543

--- src/nnue/evaluate_nnue.cpp
+++ src/nnue/evaluate_nnue.cpp
@@ -166,7 +166,7 @@ namespace Stockfish::Eval::NNUE {
     const auto positional = network[bucket]->propagate(transformedFeatures);
 
     if (complexity)
-        *complexity = (abs(psqt - positional) + abs(positional)) / OutputScale;
+        *complexity = abs(psqt - positional) / OutputScale;
 
     // Give more value to positional evaluation when adjusted flag is set
     if (adjusted)
