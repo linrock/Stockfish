a9a31ff85cec697eb4ab11f2bda385209c8995b3
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-29 19:50:42 -0400

    tune 9 search values
    bench 1350831

--- src/search.cpp
+++ src/search.cpp
@@ -38,6 +38,26 @@
 
 namespace Stockfish {
 
+  int TUNE_fpcEvalOffset = 197;
+  int TUNE_fpcLmrDepthMult = 248;
+  Value TUNE_seeDepthMult = Value(-205);
+  int TUNE_fpEvalOffset = 112;
+  int TUNE_fpDepthMult = 138;
+  int TUNE_negSeeDepthMultSq = -27;
+  int TUNE_negSeeDepthMult = 16;
+  TUNE(SetRange(0, 394), TUNE_fpcEvalOffset);
+  TUNE(SetRange(0, 496), TUNE_fpcLmrDepthMult);
+  TUNE(SetRange(-410, 0), TUNE_seeDepthMult);
+  TUNE(SetRange(0, 224), TUNE_fpEvalOffset);
+  TUNE(SetRange(0, 276), TUNE_fpDepthMult);
+  TUNE(SetRange(-54, 0), TUNE_negSeeDepthMultSq);
+  TUNE(SetRange(0, 32), TUNE_negSeeDepthMult);
+
+  int TUNE_histDenom = 7011;
+  int TUNE_chpruneDMult = -3832;
+  TUNE(SetRange(4000, 10000), TUNE_histDenom);
+  TUNE(SetRange(-7600, -1000), TUNE_chpruneDMult);
+
 namespace Search {
 
   LimitsType Limits;
@@ -985,13 +1005,13 @@ moves_loop: // When in check, search starts here
               if (   !givesCheck
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 197 + 248 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + TUNE_fpcEvalOffset + TUNE_fpcLmrDepthMult * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 7 < alpha)
                   continue;
 
               Bitboard occupied;
               // SEE based pruning (~11 Elo)
-              if (!pos.see_ge(move, occupied, Value(-205) * depth))
+              if (!pos.see_ge(move, occupied, TUNE_seeDepthMult * depth))
               {
                  if (depth < 2 - capture)
                     continue;
@@ -1020,24 +1040,24 @@ moves_loop: // When in check, search starts here
 
               // Continuation history based pruning (~2 Elo)
               if (   lmrDepth < 6
-                  && history < -3832 * depth)
+                  && history < TUNE_chpruneDMult * depth)
                   continue;
 
               history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-              lmrDepth += history / 7011;
+              lmrDepth += history / TUNE_histDenom;
               lmrDepth = std::max(lmrDepth, -2);
 
               // Futility pruning: parent node (~13 Elo)
               if (   !ss->inCheck
                   && lmrDepth < 12
-                  && ss->staticEval + 112 + 138 * lmrDepth <= alpha)
+                  && ss->staticEval + TUNE_fpEvalOffset + TUNE_fpDepthMult * lmrDepth <= alpha)
                   continue;
 
               lmrDepth = std::max(lmrDepth, 0);
 
               // Prune moves with negative SEE (~4 Elo)
-              if (!pos.see_ge(move, Value(-27 * lmrDepth * lmrDepth - 16 * lmrDepth)))
+              if (!pos.see_ge(move, Value(TUNE_negSeeDepthMultSq * lmrDepth * lmrDepth - TUNE_negSeeDepthMult * lmrDepth)))
                   continue;
           }
       }
