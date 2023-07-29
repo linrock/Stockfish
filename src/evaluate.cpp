a655dd9bd6aab929f7a6aa5d6a96d888ee2f33b9
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-29 12:50:11 -0400

    5p tune 30+0.3
    bench 1683183

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -54,16 +54,11 @@ using namespace std;
 
 namespace Stockfish {
 
-  int TUNE_pawnMult = 100;
-  int TUNE_knightMult = 325;
-  int TUNE_bishopMult = 350;
-  int TUNE_rookMult = 500;
-  int TUNE_queenMult = 900;
-  TUNE(SetRange(0, 200), TUNE_pawnMult);
-  TUNE(SetRange(0, 650), TUNE_knightMult);
-  TUNE(SetRange(0, 700), TUNE_bishopMult);
-  TUNE(SetRange(200, 800), TUNE_rookMult);
-  TUNE(SetRange(600, 1200), TUNE_queenMult);
+constexpr   int TUNE_pawnMult = 128;
+constexpr   int TUNE_knightMult = 470;
+constexpr   int TUNE_bishopMult = 189;
+constexpr   int TUNE_rookMult = 455;
+constexpr   int TUNE_queenMult = 880;
 
 namespace Eval {
 
