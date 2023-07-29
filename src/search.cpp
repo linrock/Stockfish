e8cf00316e7f1c1c9f8300c3768fdd18dce5d45a
Linmiao Xu <linmiao.xu@gmail.com>
2023-07-29 13:35:17 -0400

    Increase qm ordering bonus mult
    bench 1632316

--- src/search.cpp
+++ src/search.cpp
@@ -740,7 +740,7 @@ namespace {
     // Use static evaluation difference to improve quiet move ordering (~4 Elo)
     if (is_ok((ss-1)->currentMove) && !(ss-1)->inCheck && !priorCapture)
     {
-        int bonus = std::clamp(-18 * int((ss-1)->staticEval + ss->staticEval), -1817, 1817);
+        int bonus = std::clamp(-20 * int((ss-1)->staticEval + ss->staticEval), -2000, 2000);
         thisThread->mainHistory[~us][from_to((ss-1)->currentMove)] << bonus;
     }
 
