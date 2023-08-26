68638754ed4b24dbb15df759c908bec2111cc159
Linmiao Xu <linmiao.xu@gmail.com>
2023-08-26 09:29:06 -0400

    Always use nnue

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1111,8 +1111,7 @@ Value Eval::evaluate(const Position& pos) {
 
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
-  bool useClassical = (pos.this_thread()->depth > 9 || pos.count<ALL_PIECES>() > 7) &&
-          abs(eg_value(pos.psq_score())) * 5 > (856 + pos.non_pawn_material() / 64) * (10 + pos.rule50_count());
+  bool useClassical = false;
 
   // Deciding between classical and NNUE eval (~10 Elo): for high PSQ imbalance we use classical,
   // but we switch to NNUE during long shuffling or with high material on the board.
