3e472f2c9b9d05e99cbeef737cf8860cdc354946
Linmiao Xu <linmiao.xu@gmail.com>
2023-08-28 00:46:38 -0400

    tune 5p after turbulence
    bench 1426295

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -54,18 +54,14 @@ using namespace std;
 
 namespace Stockfish {
 
-  int TUNE_pawnMat = 126;
+  int TUNE_npmMult = 32;
   int TUNE_nnueNpmOffset = 915;
   int TUNE_optNpmOffset = 154;
-  int TUNE_nnuePawnMult = 9;
-  int TUNE_optPawnMult = 1;
   int TUNE_optMult = 2048;
   int TUNE_nnueDecayMult = 32;
-  TUNE(SetRange(0, 252), TUNE_pawnMat);
+  TUNE(SetRange(0, 64), TUNE_npmMult);
   TUNE(SetRange(615, 1215), TUNE_nnueNpmOffset);
   TUNE(SetRange(0, 308), TUNE_optNpmOffset);
-  TUNE(SetRange(0, 32), TUNE_nnuePawnMult);
-  TUNE(SetRange(0, 32), TUNE_optPawnMult);
   TUNE(SetRange(0, 4096), TUNE_optMult);
   TUNE(SetRange(0, 64), TUNE_nnueDecayMult);
 
@@ -160,7 +156,7 @@ Value Eval::evaluate(const Position& pos) {
   Value v;
 
   int nnueComplexity;
-  int npm = pos.non_pawn_material() / 64;
+  int npm = TUNE_npmMult * pos.non_pawn_material() / 2048;
 
   Color stm = pos.side_to_move();
   Value optimism = pos.this_thread()->optimism[stm];
@@ -168,14 +164,14 @@ Value Eval::evaluate(const Position& pos) {
   Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
   int material =  pos.non_pawn_material(stm) - pos.non_pawn_material(~stm)
-                + TUNE_pawnMat * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
+                + 126 * (pos.count<PAWN>(stm) - pos.count<PAWN>(~stm));
 
   // Blend optimism and eval with nnue complexity and material imbalance
   optimism += TUNE_optMult * optimism * (nnueComplexity + abs(material - nnue)) / 1048576;
   nnue     -= TUNE_nnueDecayMult * nnue * (nnueComplexity + abs(material - nnue)) / 1048576;
 
-  v = (  nnue     * (TUNE_nnueNpmOffset + npm + TUNE_nnuePawnMult * pos.count<PAWN>())
-       + optimism * (TUNE_optNpmOffset  + npm + TUNE_optPawnMult  * pos.count<PAWN>())) / 1024;
+  v = (  nnue     * (TUNE_nnueNpmOffset + npm + 9 * pos.count<PAWN>())
+       + optimism * (TUNE_optNpmOffset  + npm +     pos.count<PAWN>())) / 1024;
 
   // Damp down the evaluation linearly when shuffling
   v = v * (200 - pos.rule50_count()) / 214;
