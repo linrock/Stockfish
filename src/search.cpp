30050f3cf2ddf4d51af3c1691b5f1c69d99bd14f
Linmiao Xu <linmiao.xu@gmail.com>
2023-09-28 20:29:08 -0400

    Tune 5p eval, 1p search opt
    bench 1453057

--- src/search.cpp
+++ src/search.cpp
@@ -47,6 +47,9 @@
 
 namespace Stockfish {
 
+          int TUNE_optNum = 109;
+          TUNE(SetRange(0, 218), TUNE_optNum);
+
 namespace Search {
 
   LimitsType Limits;
@@ -364,7 +367,7 @@ void Thread::search() {
           beta  = std::min(prev + delta, VALUE_INFINITE);
 
           // Adjust optimism based on root move's previousScore (~4 Elo)
-          int opt = 109 * prev / (std::abs(prev) + 141);
+          int opt = TUNE_optNum * prev / (std::abs(prev) + 141);
           optimism[ us] = Value(opt);
           optimism[~us] = -optimism[us];
 
