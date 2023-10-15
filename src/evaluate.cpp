3d0acf9860b194b1c7993252738aad0e797b360d
Linmiao Xu <linmiao.xu@gmail.com>
2023-10-15 13:58:15 -0400

    6 tuned eval/opt values, pre-BST opt
    bench 1136957

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -158,9 +158,10 @@ Value Eval::evaluate(const Position& pos) {
   int simpleEval = simple_eval(pos, stm) + (int(pos.key() & 7) - 3);
 
   bool lazy = abs(simpleEval) >=   RookValue + KnightValue
-                                 + 16 * shuffling * shuffling
+                                 + 20 * shuffling * shuffling
                                  + abs(pos.this_thread()->bestValue)
-                                 + abs(pos.this_thread()->rootSimpleEval);
+                                 + abs(pos.this_thread()->rootSimpleEval)
+                                 - 443;
 
   if (lazy)
       v = Value(simpleEval);
@@ -176,8 +177,8 @@ Value Eval::evaluate(const Position& pos) {
       nnue     -= nnue     * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
       int npm = pos.non_pawn_material() / 64;
-      v = (  nnue     * (915 + npm + 9 * pos.count<PAWN>())
-           + optimism * (154 + npm                       )) / 1024;
+      v = (  nnue     * (978 + npm + 7 * pos.count<PAWN>())
+           + optimism * (124 + npm                       )) / 1024;
   }
 
   // Damp down the evaluation linearly when shuffling
