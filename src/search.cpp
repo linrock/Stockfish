b87d795ba22f2e493adafbf45fbf1706cf5b2f8f
Linmiao Xu <linmiao.xu@gmail.com>
2023-10-20 12:10:57 -0400

    prev stat score denominator
    bench 1150204

--- src/search.cpp
+++ src/search.cpp
@@ -1178,7 +1178,7 @@ moves_loop: // When in check, search starts here
                      - 3848;
 
       // Decrease/increase reduction for moves with a good/bad history (~25 Elo)
-      r -= ss->statScore / (10216 + 3855 * (depth > 5 && depth < 23));
+      r -= ss->statScore / (11124 + 4740 * (depth > 5 && depth < 22));
 
       // Step 17. Late moves reduction / extension (LMR, ~117 Elo)
       // We use various heuristics for the sons of a node after the first son has
