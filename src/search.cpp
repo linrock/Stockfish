b85680686285a4702d97d6e047430f0876a31e24
Linmiao Xu <linmiao.xu@gmail.com>
2023-10-23 19:31:31 -0400

    tune 6 search params
    bench 1330590

--- src/search.cpp
+++ src/search.cpp
@@ -47,6 +47,20 @@
 
 namespace Stockfish {
 
+  int TUNE_histDepthMult = -3645;
+  int TUNE_fpcEvalOffset = 239;
+  int TUNE_fpcLmrDepthMult = 291;
+  int TUNE_histDenom = 7836;
+  int TUNE_fpEvalOffset = 77;
+  int TUNE_futBaseOffset = 200;
+
+  TUNE(SetRange(0, 394), TUNE_fpcEvalOffset);
+  TUNE(SetRange(0, 412), TUNE_fpcLmrDepthMult);
+  TUNE(SetRange(-7290, -1822), TUNE_histDepthMult);
+  TUNE(SetRange(2836, 11536), TUNE_histDenom);
+  TUNE(SetRange(0, 154), TUNE_fpEvalOffset);
+  TUNE(SetRange(0, 400), TUNE_futBaseOffset);
+
 namespace Search {
 
 LimitsType Limits;
@@ -776,7 +790,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
                - (ss - 1)->statScore / 321
              >= beta
         && eval >= beta && eval < 29462  // smaller than TB wins
-        && (!ttMove || ttCapture))
+        && !(!ttCapture && ttMove))
         return eval;
 
     // Step 9. Null move search with verification search (~35 Elo)
@@ -982,7 +996,7 @@ moves_loop:  // When in check, search starts here
                 {
                     Piece capturedPiece = pos.piece_on(to_sq(move));
                     int   futilityEval =
-                      ss->staticEval + 239 + 291 * lmrDepth + PieceValue[capturedPiece]
+                      ss->staticEval + TUNE_fpcEvalOffset + TUNE_fpcLmrDepthMult * lmrDepth + PieceValue[capturedPiece]
                       + captureHistory[movedPiece][to_sq(move)][type_of(capturedPiece)] / 7;
                     if (futilityEval < alpha)
                         continue;
@@ -1000,16 +1014,16 @@ moves_loop:  // When in check, search starts here
                             + thisThread->pawnHistory[pawn_structure(pos)][movedPiece][to_sq(move)];
 
                 // Continuation history based pruning (~2 Elo)
-                if (lmrDepth < 6 && history < -3645 * depth)
+                if (lmrDepth < 6 && history < TUNE_histDepthMult * depth)
                     continue;
 
                 history += 2 * thisThread->mainHistory[us][from_to(move)];
 
-                lmrDepth += history / 7836;
+                lmrDepth += history / TUNE_histDenom;
                 lmrDepth = std::max(lmrDepth, -1);
 
                 // Futility pruning: parent node (~13 Elo)
-                if (!ss->inCheck && lmrDepth < 13 && ss->staticEval + 77 + 124 * lmrDepth <= alpha)
+                if (!ss->inCheck && lmrDepth < 13 && ss->staticEval + TUNE_fpEvalOffset + 124 * lmrDepth <= alpha)
                     continue;
 
                 lmrDepth = std::max(lmrDepth, 0);
@@ -1460,7 +1474,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = ss->staticEval + 200;
+        futilityBase = ss->staticEval + TUNE_futBaseOffset;
     }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
