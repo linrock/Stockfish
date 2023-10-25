ef2611ceedaef819144cc7a52b06b4e19b115be1
Linmiao Xu <linmiao.xu@gmail.com>
2023-10-25 11:10:26 -0400

    futility base eval offset 173
    bench 1325824

--- src/search.cpp
+++ src/search.cpp
@@ -1451,7 +1451,7 @@ Value qsearch(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth) {
         if (bestValue > alpha)
             alpha = bestValue;
 
-        futilityBase = ss->staticEval + 200;
+        futilityBase = ss->staticEval + 173;
     }
 
     const PieceToHistory* contHist[] = {(ss - 1)->continuationHistory,
