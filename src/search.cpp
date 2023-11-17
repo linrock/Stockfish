a54f3790cd9d5ccabdb1327ecd67683723feaac8
Linmiao Xu <linmiao.xu@gmail.com>
2023-11-17 08:25:49 -0500

    6p stat bonus/malus
    bench 1335558

--- src/search.cpp
+++ src/search.cpp
@@ -94,10 +94,10 @@ constexpr int futility_move_count(bool improving, Depth depth) {
 }
 
 // History and stats update bonus, based on depth
-int stat_bonus(Depth d) { return std::min(364 * d - 438, 1501); }
+int stat_bonus(Depth d) { return std::min(310 * d - 277, 1470); }
 
 // History and stats update malus, based on depth
-int stat_malus(Depth d) { return std::min(452 * d - 452, 1478); }
+int stat_malus(Depth d) { return std::min(499 * d - 454, 1417); }
 
 // Add a small random component to draw evaluations to avoid 3-fold blindness
 Value value_draw(const Thread* thisThread) {
