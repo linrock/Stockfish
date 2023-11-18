af0b91cc6551b818f6f4b09bc48cdf150bd1337d
Linmiao Xu <linmiao.xu@gmail.com>
2023-11-18 13:44:05 -0500

    rm opt offset, complexity 256, nnue 960, pc 10x
    bench 1249132

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -178,11 +178,11 @@ Value Eval::evaluate(const Position& pos) {
         Value optimism = pos.this_thread()->optimism[stm];
 
         // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + abs(simpleEval - nnue)) / 512;
+        optimism += optimism * (nnueComplexity + abs(simpleEval - nnue)) / 256;
         nnue -= nnue * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (960 + npm + 9 * pos.count<PAWN>()) + optimism * npm) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
