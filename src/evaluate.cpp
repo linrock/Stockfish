80c9c88054f009733781a95edc2fdbeba5169753
Linmiao Xu <linmiao.xu@gmail.com>
2023-11-24 12:44:56 -0500

    6p complexity, damped
    bench 1637551

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,13 +176,14 @@ Value Eval::evaluate(const Position& pos) {
         Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
         Value optimism = pos.this_thread()->optimism[stm];
+        int complexity = 16 * nnueComplexity + 56 * abs(simpleEval - nnue);
 
         // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + abs(simpleEval - nnue)) / 512;
-        nnue -= nnue * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
+        optimism += optimism * complexity / 16384;
+        nnue -= nnue * complexity / 1048576;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (1144 + npm + 9 * pos.count<PAWN>()) + optimism * (142 + npm)) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
