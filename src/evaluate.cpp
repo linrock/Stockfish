9081bc4bae9ec1da85e21b0be277b64ab0d79176
Linmiao Xu <linmiao.xu@gmail.com>
2023-11-26 18:07:19 -0500

    weigh nnue complexity more
    bench 1394664

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -176,10 +176,11 @@ Value Eval::evaluate(const Position& pos) {
         Value nnue = NNUE::evaluate(pos, true, &nnueComplexity);
 
         Value optimism = pos.this_thread()->optimism[stm];
+        int complexity = (36 * nnueComplexity + 28 * abs(simpleEval - nnue)) / 32;
 
         // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + abs(simpleEval - nnue)) / 512;
-        nnue -= nnue * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
+        optimism += optimism * complexity / 512;
+        nnue -= nnue * complexity / 32768;
 
         int npm = pos.non_pawn_material() / 64;
         v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
