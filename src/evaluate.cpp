896faf0f2ba7511cc708479463e58bc032bfe111
Linmiao Xu <linmiao.xu@gmail.com>
2023-11-30 14:15:37 -0500

    bench 1463074

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -166,7 +166,8 @@ Value Eval::evaluate(const Position& pos) {
 
     bool lazy = abs(simpleEval) >= RookValue + KnightValue + 16 * shuffling * shuffling
                                      + abs(pos.this_thread()->bestValue)
-                                     + abs(pos.this_thread()->rootSimpleEval);
+                                     + abs(pos.this_thread()->rootSimpleEval)
+                                     - 343;
 
     if (lazy)
         v = Value(simpleEval);
@@ -182,7 +183,7 @@ Value Eval::evaluate(const Position& pos) {
         nnue -= nnue * (nnueComplexity + abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (948 + npm + 8 * pos.count<PAWN>()) + optimism * (60 + npm)) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
