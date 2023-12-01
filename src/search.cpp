3e2c1496897ec0c63006ac00d5c5f7da8ee10027
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-01 11:58:31 -0500

    Remove constant in null move eval threshold
    bench 1008050

--- src/search.cpp
+++ src/search.cpp
@@ -784,7 +784,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
 
     // Step 9. Null move search with verification search (~35 Elo)
     if (!PvNode && (ss - 1)->currentMove != MOVE_NULL && (ss - 1)->statScore < 17257 && eval >= beta
-        && eval >= ss->staticEval && ss->staticEval >= beta - 24 * depth + 281 && !excludedMove
+        && eval >= ss->staticEval && ss->staticEval >= beta - 16 * depth && !excludedMove
         && pos.non_pawn_material(us) && ss->ply >= thisThread->nmpMinPly
         && beta > VALUE_TB_LOSS_IN_MAX_PLY)
     {
