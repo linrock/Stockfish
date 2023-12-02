f49dba6ceea888e79c22238e2692845fdafc089e
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-02 13:42:31 -0500

    never lazy when in check
    bench 1325861

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -157,7 +157,7 @@ Value Eval::simple_eval(const Position& pos, Color c) {
 // of the position from the point of view of the side to move.
 Value Eval::evaluate(const Position& pos) {
 
-    assert(!pos.checkers());
+    // assert(!pos.checkers());
 
     Value v;
     Color stm        = pos.side_to_move();
@@ -168,7 +168,7 @@ Value Eval::evaluate(const Position& pos) {
                                      + abs(pos.this_thread()->bestValue)
                                      + abs(pos.this_thread()->rootSimpleEval);
 
-    if (lazy)
+    if (lazy && !pos.checkers())
         v = Value(simpleEval);
     else
     {
