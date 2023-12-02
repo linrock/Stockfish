0264c4f45ab682e57ed6749c616a1cf117e8cb33
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-01 19:14:58 -0500

    remove eval in nm reduction
    bench 1236572

--- src/search.cpp
+++ src/search.cpp
@@ -791,7 +791,7 @@ Value search(Position& pos, Stack* ss, Value alpha, Value beta, Depth depth, boo
         assert(eval - beta >= 0);
 
         // Null move dynamic reduction based on depth and eval
-        Depth R = std::min(int(eval - beta) / 152, 6) + depth / 3 + 4;
+        Depth R = depth / 3 + 10;
 
         ss->currentMove         = MOVE_NULL;
         ss->continuationHistory = &thisThread->continuationHistory[0][0][NO_PIECE][0];
