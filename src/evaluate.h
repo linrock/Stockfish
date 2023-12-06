155698cc2ecda3037fbda3203fb45adfc4477ed2
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-06 10:25:45 -0500

    different S2 hse 149G net
    bench 1476661

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ extern std::string currentEvalFileName[2];
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro, as it is used in the Makefile.
 #define EvalFileDefaultNameBig   "nn-0000000000a0.nnue"
-#define EvalFileDefaultNameSmall "nn-cab34908e684.nnue" // S2 ep499 perm leela-v2 + 87G hse
+#define EvalFileDefaultNameSmall "nn-a378c9c91bb0.nnue" // S2 ep759 perm leela-v2 + 149G hse
 
 namespace NNUE {
 
