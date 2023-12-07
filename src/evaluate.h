e7d36613ef728e34f32b17b4b5fdfc96759d4064
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-06 23:43:52 -0500

    nn-2de36b0051d3.nnue
    bench 1641262

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ extern std::string currentEvalFileName;
 // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro, as it is used in the Makefile.
-#define EvalFileDefaultName "nn-ecb35f70ff2a.nnue"
+#define EvalFileDefaultName "nn-2de36b0051d3.nnue"
 
 namespace NNUE {
 
