8ee00d5a601c0048b9d610abf292b779cc8f483b
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-16 11:12:08 -0500

    1100 < smallnet < 2700
    bench 1307385

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -170,7 +170,7 @@ Value Eval::evaluate(const Position& pos) {
     int   simpleEval = pos.simple_eval();
 
     int lazyThresholdSimpleEval = 2800;
-    int lazyThresholdSmallNet = 1000;
+    int lazyThresholdSmallNet = 1100;
 
     bool lazy = abs(simpleEval) > lazyThresholdSimpleEval;
     if (lazy)
