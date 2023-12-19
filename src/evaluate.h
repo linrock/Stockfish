2fe30ff5481dfc638b78744eb2830a440fe94dc6
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-19 01:20:55 -0500

    nn-9ac4144b3ffa.nnue
    bench 1160203

--- src/evaluate.h
+++ src/evaluate.h
@@ -41,7 +41,7 @@ extern std::string currentEvalFileName[2];
 #define EvalFileDefaultNameBig   "nn-0000000000a0.nnue"
 
 // S1 ep399 perm hse-only v2
-#define EvalFileDefaultNameSmall "nn-c01dc0ffeede.nnue"
+#define EvalFileDefaultNameSmall "nn-9ac4144b3ffa.nnue"
 
 namespace NNUE {
 
