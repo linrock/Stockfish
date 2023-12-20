4c4cce78698671fff6e39e39ce6ed616038b8b60
Linmiao Xu <linmiao.xu@gmail.com>
2023-12-19 19:27:01 -0500

    1050 < smallnet < 2700 with 2p eval
    bench 1130746

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -192,7 +192,7 @@ Value Eval::evaluate(const Position& pos) {
         nnue -= nnue * (nnueComplexity + std::abs(simpleEval - nnue)) / 32768;
 
         int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (915 + npm + 9 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
+        v       = (nnue * (1015 + npm + 7 * pos.count<PAWN>()) + optimism * (154 + npm)) / 1024;
     }
 
     // Damp down the evaluation linearly when shuffling
