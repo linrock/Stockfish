e00383e050f139fadc926c14300dcb34d96f7dab
Linmiao Xu <linmiao.xu@gmail.com>
2024-01-15 00:07:42 -0500

    ep899 2560--S10-514G-T80-augtodec2023-skip-more-wdl-20p-more-loss-high-q-pow23-sk28
    bench 1286012

--- src/evaluate.h
+++ src/evaluate.h
@@ -39,7 +39,7 @@ Value evaluate(const Position& pos, int optimism);
 // The default net name MUST follow the format nn-[SHA256 first 12 digits].nnue
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro, as it is used in the Makefile.
-#define EvalFileDefaultNameBig "nn-baff1edbea57.nnue"
+#define EvalFileDefaultNameBig "nn-dce704c99e54.nnue"
 #define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
 
 struct EvalFile {
