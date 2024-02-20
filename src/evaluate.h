6457a7d34e91d00165fd483e8b98574036139356
Linmiao Xu <linmiao.xu@gmail.com>
2024-02-19 20:25:40 -0500

    nn-3782c3234054.nnue
    bench 1386476

--- src/evaluate.h
+++ src/evaluate.h
@@ -40,7 +40,7 @@ Value evaluate(const Position& pos, int optimism);
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro, as it is used in the Makefile.
 #define EvalFileDefaultNameBig "nn-b1a57edbea57.nnue"
-#define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
+#define EvalFileDefaultNameSmall "nn-3782c3234054.nnue"
 
 struct EvalFile {
     // UCI option name
