05feb2f1481986f7b772c5786f668d25662acf7f
Linmiao Xu <linmiao.xu@gmail.com>
2024-04-04 10:17:25 -0400

    nn-122b7b6698e9.nnue
    bench 1904841

--- src/evaluate.h
+++ src/evaluate.h
@@ -36,7 +36,7 @@ constexpr inline int SmallNetThreshold = 1165, PsqtOnlyThreshold = 2500;
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
 #define EvalFileDefaultNameBig "nn-ae6a388e4a1a.nnue"
-#define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
+#define EvalFileDefaultNameSmall "nn-122b7b6698e9.nnue"
 
 namespace NNUE {
 struct Networks;
