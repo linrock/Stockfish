e10b39a0ede181979e3d2b6ad83ef66118c4d930
Linmiao Xu <linmiao.xu@gmail.com>
2024-04-13 15:34:11 -0400

    Try diff damping between net types
    bench 1443072

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -58,29 +58,21 @@ Value Eval::evaluate(const Eval::NNUE::Networks& networks, const Position& pos, 
     Value nnue = smallNet ? networks.small.evaluate(pos, true, &nnueComplexity, psqtOnly)
                           : networks.big.evaluate(pos, true, &nnueComplexity, false);
 
-    const auto adjustEval = [&](int optDiv, int nnueDiv, int pawnCountConstant, int pawnCountMul,
-                                int npmConstant, int evalDiv, int shufflingConstant,
-                                int shufflingDiv) {
-        // Blend optimism and eval with nnue complexity and material imbalance
-        optimism += optimism * (nnueComplexity + std::abs(simpleEval - nnue)) / optDiv;
-        nnue -= nnue * (nnueComplexity * 5 / 3) / nnueDiv;
+    // Blend optimism and eval with nnue complexity and material imbalance
+    optimism += optimism * (nnueComplexity + std::abs(simpleEval - nnue)) / 512;
+    nnue -= nnue * (nnueComplexity * 5 / 3) / 32768;
 
-        int npm = pos.non_pawn_material() / 64;
-        v       = (nnue * (npm + pawnCountConstant + pawnCountMul * pos.count<PAWN>())
-             + optimism * (npmConstant + npm))
-          / evalDiv;
+    int npm = pos.non_pawn_material() / 64;
+    v       = (nnue * (npm + 910 + 8 * pos.count<PAWN>()) + optimism * (npm + 150)) / 1024;
 
-        // Damp down the evaluation linearly when shuffling
-        int shuffling = pos.rule50_count();
-        v             = v * (shufflingConstant - shuffling) / shufflingDiv;
-    };
-
+    // Damp down the evaluation linearly when shuffling
+    int shuffling = pos.rule50_count();
     if (!smallNet)
-        adjustEval(513, 32395, 919, 11, 145, 1036, 178, 204);
+        v = v * (178 - shuffling) / 204;
     else if (psqtOnly)
-        adjustEval(517, 32857, 908, 7, 155, 1019, 224, 238);
+        v = v * (224 - shuffling) / 238;
     else
-        adjustEval(499, 32793, 903, 9, 147, 1067, 208, 211);
+        v = v * (208 - shuffling) / 211;
 
     // Guarantee evaluation does not hit the tablebase range
     v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
