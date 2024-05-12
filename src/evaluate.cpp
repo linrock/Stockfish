a9cc116dbe63554c0b1898eb78bcd1a7ee5d9033
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-12 19:12:19 -0400

    larger smallnet range for reEval
    bench 2180956

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -62,8 +62,10 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     Value nnue = smallNet ? networks.small.evaluate(pos, &caches.small, true, &nnueComplexity)
                           : networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
 
-    if (smallNet && (nnue * simpleEval < 0 || abs(nnue) < 500))
+    if (smallNet && (nnue * simpleEval < 0 || abs(nnue) < 1000)) {
         nnue = networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
+        smallNet = false;
+    }
 
     const auto adjustEval = [&](int nnueDiv, int pawnCountConstant, int pawnCountMul,
                                 int npmConstant, int evalDiv, int shufflingConstant) {
