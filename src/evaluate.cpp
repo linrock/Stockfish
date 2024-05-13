e46df5fd993803303cb374593209ce848b4cfcad
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-12 20:54:48 -0400

    1/32 with nnue < 250
    bench 2540741

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -62,7 +62,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     Value nnue = smallNet ? networks.small.evaluate(pos, &caches.small, true, &nnueComplexity)
                           : networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
 
-    if (smallNet && ((nnue * simpleEval < 0 || abs(nnue) < 500) || (pos.key() & 31) == 0))
+    if (smallNet && ((nnue * simpleEval < 0 || abs(nnue) < 250) || (pos.key() & 31) == 0))
         nnue = networks.big.evaluate(pos, &caches.big, true, &nnueComplexity);
 
     const auto adjustEval = [&](int nnueDiv, int pawnCountConstant, int pawnCountMul,
