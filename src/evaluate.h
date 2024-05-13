22da34cb56f1cc335e480f06cd7da2ac44ec65f4
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-12 23:48:15 -0400

    Switch to master main net
    bench 1508337

--- src/evaluate.h
+++ src/evaluate.h
@@ -35,7 +35,7 @@ constexpr inline int SmallNetThreshold = 1274;
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-f85738aefa84.nnue"
+#define EvalFileDefaultNameBig "nn-ae6a388e4a1a.nnue"
 #define EvalFileDefaultNameSmall "nn-baff1ede1f90.nnue"
 
 namespace NNUE {
