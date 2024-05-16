1c7533842f26fc0eb2575dd25f0dcb5787446247
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-16 01:49:41 -0400

    Numbers in same place

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -60,8 +60,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 }
 
 bool Eval::use_smallnet(const Position& pos) {
-    int  simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > SmallNetThreshold + 6 * pos.count<PAWN>();
+    int simpleEval = simple_eval(pos, pos.side_to_move());
+    return std::abs(simpleEval) > 1126 + 6 * pos.count<PAWN>();
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
