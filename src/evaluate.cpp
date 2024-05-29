2534e5c8279a94ca16f0da99996182dcf4e0eaa0
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-29 14:12:46 -0400

    sn thresh quadratic, concave down
    bench 1189810

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,8 +46,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    int pc = pos.count<PAWN>();
-    return std::abs(simpleEval) > 992 + 6 * pc * pc / 16;
+    const int pc = pos.count<PAWN>();
+    return std::abs(simpleEval) > 992 - 3 * pc * pc / 8 + 12 * pc;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
