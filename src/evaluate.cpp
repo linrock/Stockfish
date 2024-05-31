e74c409633184678ece94e43334e808afc03d183
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-31 19:32:40 -0400

    even steeper drop
    bench 1272569

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,7 @@ int Eval::simple_eval(const Position& pos, Color c) {
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
     int pawnCount  = pos.count<PAWN>();
-    return std::abs(simpleEval) > 992 + 7 * pawnCount * pawnCount / 16 - pawnCount;
+    return std::abs(simpleEval) > 992 + (8 * pawnCount * pawnCount / 16) - (7 * pawnCount / 4);
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
