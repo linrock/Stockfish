9913fdbce7cfaa607e698e0c15db389f0ba18a8f
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-01 17:55:44 -0400

    spsa 111k, round pc up
    bench 1180861

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,7 @@ int Eval::simple_eval(const Position& pos, Color c) {
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
     int pawnCount  = pos.count<PAWN>();
-    return std::abs(simpleEval) > 952 + 6 * pawnCount * pawnCount / 16 + pawnCount;
+    return std::abs(simpleEval) > 952 + 7 * pawnCount * pawnCount / 16 + 2 * pawnCount;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
@@ -78,10 +78,10 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 470;
     nnue -= nnue * nnueComplexity / 20000;
 
-    int material = 275 * pos.count<PAWN>() + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
+    int material = 265 * pos.count<PAWN>() + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
                  + 640 * pos.count<ROOK>() + 1200 * pos.count<QUEEN>();
 
-    v = (nnue * (34300 + material) + optimism * (4400 + material)) / 34542;
+    v = (nnue * (34300 + material) + optimism * (4400 + material)) / 34623;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
