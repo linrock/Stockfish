2be9192e56771a121a5475dfa4cb42eb970dd912
Linmiao Xu <linmiao.xu@gmail.com>
2024-05-31 20:04:13 -0400

    quadratic pawn count material
    bench 1334087

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -77,7 +77,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 470;
     nnue -= nnue * nnueComplexity / 20000;
 
-    int material = 300 * pos.count<PAWN>() + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
+    int pawnCount = pos.count<PAWN>();
+    int material = 300 * pawnCount * pawnCount / 16 + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
                  + 640 * pos.count<ROOK>() + 1200 * pos.count<QUEEN>();
 
     v = (nnue * (34300 + material) + optimism * (4400 + material)) / 35967;
