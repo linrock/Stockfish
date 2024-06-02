1275eb9a6b41ca3e64c15e3a838cef962d8c351e
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-01 23:10:31 -0400

    only sn thresh change
    bench 1035434

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -78,10 +78,10 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     optimism += optimism * nnueComplexity / 470;
     nnue -= nnue * nnueComplexity / 20000;
 
-    int material = 275 * pos.count<PAWN>() + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
+    int material = 300 * pos.count<PAWN>() + 350 * pos.count<KNIGHT>() + 400 * pos.count<BISHOP>()
                  + 640 * pos.count<ROOK>() + 1200 * pos.count<QUEEN>();
 
-    v = (nnue * (34300 + material) + optimism * (4400 + material)) / 34542;
+    v = (nnue * (34300 + material) + optimism * (4400 + material)) / 36672;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
