cb9fd04f13d85da81975a3f409ca7070d0b01a9f
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-02 02:42:45 -0400

    try rm dds const
    bench 1275561

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop:  // When in check, search starts here
             {
                 // Adjust full-depth search based on LMR results - if the result
                 // was good enough search deeper, if it was bad enough search shallower.
-                const bool doDeeperSearch    = value > (bestValue + 36 + 2 * newDepth);  // (~1 Elo)
+                const bool doDeeperSearch    = value > bestValue + 4 * newDepth;         // (~1 Elo)
                 const bool doShallowerSearch = value < bestValue + newDepth;             // (~2 Elo)
 
                 newDepth += doDeeperSearch - doShallowerSearch;
