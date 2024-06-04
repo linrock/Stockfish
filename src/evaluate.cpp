847f3467105a201c7289fd9640393ecf12d5d180
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-03 17:58:30 -0700

    between quad and linear
    bench 1306418

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 992 + 10 * pos.count<PAWN>() * pos.count<PAWN>() / 16;
+    int pawnCount = pos.count<PAWN>();
+    return std::abs(simpleEval) > 992 + 10 * pawnCount * pawnCount / 32 + 5 * pawnCount;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
