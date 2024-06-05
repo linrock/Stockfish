84625f2ffc687a85fe7d0b629557850c8c597a76
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-05 15:43:06 -0700

    Fix after rebase

--- src/nnue/network.h
+++ src/nnue/network.h
@@ -73,7 +73,8 @@ class Network {
                                  AccumulatorCaches::Cache<FTDimensions>* cache) const;
 
     // Evaluation function
-    AlignedPtr<Arch> network[LayerStacks];
+    // AlignedPtr<Arch> network[LayerStacks];
+    AlignedPtr<Arch[]> network;
 
    private:
     void load_user_net(const std::string&, const std::string&);
@@ -94,7 +95,7 @@ class Network {
     LargePagePtr<Transformer> featureTransformer;
 
     // Evaluation function
-    AlignedPtr<Arch[]> network;
+    // AlignedPtr<Arch[]> network;
 
     EvalFile         evalFile;
     EmbeddedNNUEType embeddedType;
