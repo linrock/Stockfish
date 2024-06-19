d56647c7c83665570c1d709b6131083529220f67
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-18 22:20:17 -0400

    9p eval
    bench 1134281

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -37,6 +37,33 @@
 
 namespace Stockfish {
 
+    int snThresh = 962;
+    TUNE(SetRange(462, 1462), snThresh);
+
+    int snPc = 0;
+    TUNE(SetRange(-32, 32), snPc);
+
+    int reThresh = 227;
+    TUNE(SetRange(0, 454), reThresh);
+
+    int optDiv = 457;
+    TUNE(SetRange(228, 914), optDiv);
+
+    int nnueDiv = 19157;
+    TUNE(SetRange(9578, 38314), nnueDiv);
+
+    int matPc = 554;
+    TUNE(SetRange(254, 854), matPc);
+
+    int nnueMatOffset = 73921;
+    TUNE(SetRange(53921, 93921), nnueMatOffset);
+
+    int optMatOffset = 8112;
+    TUNE(SetRange(0, 16224), optMatOffset);
+
+    int evalDiv = 73260;
+    TUNE(SetRange(36630, 146520), evalDiv);
+
 // Returns a static, purely materialistic evaluation of the position from
 // the point of view of the given color. It can be divided by PawnValue to get
 // an approximation of the material advantage on the board in terms of pawns.
@@ -47,7 +74,7 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 962;
+    return std::abs(simpleEval) > snThresh + snPc;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
@@ -70,7 +97,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     int   nnueComplexity = std::abs(psqt - positional);
 
     // Re-evaluate the position when higher eval accuracy is worth the time spent
-    if (smallNet && (nnue * simpleEval < 0 || std::abs(nnue) < 227))
+    if (smallNet && (nnue * simpleEval < 0 || std::abs(nnue) < reThresh))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, &caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
@@ -79,11 +106,11 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     }
 
     // Blend optimism and eval with nnue complexity
-    optimism += optimism * nnueComplexity / 457;
-    nnue -= nnue * nnueComplexity / 19157;
+    optimism += optimism * nnueComplexity / optDiv;
+    nnue -= nnue * nnueComplexity / nnueDiv;
 
-    int material = 554 * pos.count<PAWN>() + pos.non_pawn_material();
-    v            = (nnue * (73921 + material) + optimism * (8112 + material)) / 73260;
+    int material = matPc * pos.count<PAWN>() + pos.non_pawn_material();
+    v            = (nnue * (nnueMatOffset + material) + optimism * (optMatOffset + material)) / evalDiv;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
