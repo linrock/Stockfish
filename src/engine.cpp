d29a5578124400ea48b23a6cb392a9a2a445f7d2
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-29 12:28:37 -0400

    Load spsa params from a file

--- src/engine.cpp
+++ src/engine.cpp
@@ -60,6 +60,10 @@ Engine::Engine(std::string path) :
     pos.set(StartFEN, false, &states->back());
     capSq = SQ_NONE;
 
+    // delayed addition of options
+    if (Tune::enabled)
+        options["TuneFile"] << Option("<empty>", [](const Option& o) { Tune::read_file(o); return std::nullopt; });
+
     options["Debug Log File"] << Option("", [](const Option& o) {
         start_logger(o);
         return std::nullopt;
