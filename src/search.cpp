e187b92288b04f5820d9512cb17d94c121ec445b
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-30 15:23:47 -0400

    Use opt in sn threshold

--- src/search.cpp
+++ src/search.cpp
@@ -716,7 +716,8 @@ Value Search::Worker::search(
     {
         // Providing the hint that this node's accumulator will be used often
         // brings significant Elo gain (~13 Elo).
-        Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable);
+        Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable,
+                                                thisThread->optimism[us]);
         unadjustedStaticEval = eval = ss->staticEval;
     }
     else if (ss->ttHit)
@@ -727,7 +728,8 @@ Value Search::Worker::search(
             unadjustedStaticEval =
               evaluate(networks[numaAccessToken], pos, refreshTable, thisThread->optimism[us]);
         else if (PvNode)
-            Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable);
+            Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable,
+                                                    thisThread->optimism[us]);
 
         ss->staticEval = eval = to_corrected_static_eval(unadjustedStaticEval, *thisThread, pos);
 
@@ -898,7 +900,8 @@ Value Search::Worker::search(
                 }
             }
 
-        Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable);
+        Eval::NNUE::hint_common_parent_position(pos, networks[numaAccessToken], refreshTable,
+                                                thisThread->optimism[us]);
     }
 
 moves_loop:  // When in check, search starts here
