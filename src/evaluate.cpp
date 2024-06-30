71f11f6c9880893867f5f785d87185c41d66176f
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-30 13:02:28 -0400

    even lower when no pawns
    bench 1396754

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,7 +47,7 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 962 - (pos.count<PAWN>() == 0 ? 32 : 0);
+    return std::abs(simpleEval) > 962 - (pos.count<PAWN>() == 0 ? 64 : 0);
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
