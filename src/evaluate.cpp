3e6072d915834ee6212211906be8e70e7ae73542
Linmiao Xu <linmiao.xu@gmail.com>
2024-06-30 20:53:44 -0400

    avg nc upon re
    bench 1329174

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -74,7 +74,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, &caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
-        nnueComplexity             = std::abs(psqt - positional);
+        nnueComplexity             = (nnueComplexity + std::abs(psqt - positional)) / 2;
         smallNet                   = false;
     }
 
