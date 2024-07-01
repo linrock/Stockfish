4a17e8a8edf2c9963b90ea189286669ee70344cb
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-01 12:25:06 -0400

    Script to modify new spsa params

---
+++ src/modify_spsa_params.py
@@ -0,0 +1,13 @@
+# spsa params from ./stockfish output
+with open("params-twoWB-bucketIdx-3.txt", "r") as f:
+    rows = [row.split(",") for row in f.read().strip().split("\n")]
+
+# recommended by Viren in Discord:
+# c value 4 for the weights and c value 128 for the biases
+with open("params-twoWB-bucketIdx-3-fixed-c.txt", "w") as f:
+    for row in rows:
+        if row[0].split("[")[0][-1] == "W":
+            row[4] = "4"
+        elif row[0].split("[")[0][-1] == "B":
+            row[4] = "128"
+        f.write(",".join(row) + "\n")
