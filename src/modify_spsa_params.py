f062fe6004a9128e045a8343226bb89f4ef0bbb3
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-01 23:23:08 -0400

    Generalize script

--- src/modify_spsa_params.py
+++ src/modify_spsa_params.py
@@ -1,10 +1,22 @@
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python3 modify_spsa_params.py <filename>")
+    sys.exit(0)
+
 # spsa params from ./stockfish output
-with open("params-twoW-bucketIdx-3-4.txt", "r") as f:
+filename = sys.argv[1]
+output_filename = filename.replace(".txt", ".fixed-c.txt")
+
+print(f"Modifying spsa params in: {filename}")
+with open(filename, "r") as f:
     rows = [row.split(",") for row in f.read().strip().split("\n")]
 
-# recommended by Viren in Discord:
-# c value 4 for the weights and c value 128 for the biases
-with open("params-twoW-bucketIdx-3-4-fixed-c.txt", "w") as f:
+# c value 4 for weights
+# c value 128 for biases
+print(f"Output params in: {output_filename}")
+with open(output_filename, "w") as f:
     for row in rows:
         if row[0].split("[")[0][-1] == "W":
             row[4] = "4"
