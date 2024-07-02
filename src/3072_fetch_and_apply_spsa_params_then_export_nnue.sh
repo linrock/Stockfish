09f2812c0ca918b00b0b09c333ed6a57060fa943
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-02 12:25:29 -0400

    Fix fetch, apply, export script

--- src/3072_fetch_and_apply_spsa_params_then_export_nnue.sh
+++ src/3072_fetch_and_apply_spsa_params_then_export_nnue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu -o pipefail
 
-python3 3072_fetch_spsa_params.py | tee spsa-variables-cpp.txt
+python3 3072_file_twoW_fetch_spsa_params.py | tee spsa-variables-cpp.txt
 python3 3072_apply_spsa_params.py
 
 make clean
