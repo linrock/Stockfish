72fad06fe81fac0dd2530860432d5c8e3fb72e49
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-03 00:35:45 -0400

    spsa 70%
    bench 1198759

--- src/search.cpp
+++ src/search.cpp
@@ -56,13 +56,13 @@ using namespace Search;
 namespace {
 
 
-constexpr int sbDMult = 189;
-constexpr int sbMax = 1429;
-constexpr int smDMult = 737;
-constexpr int smMax = 1920;
-constexpr int ssTh = -7923;
-constexpr int bMax = 223;
-constexpr int betaOffset = 163;
+constexpr int sbDMult = 185;
+constexpr int sbMax = 1399;
+constexpr int smDMult = 741;
+constexpr int smMax = 1956;
+constexpr int ssTh = -8111;
+constexpr int bMax = 225;
+constexpr int betaOffset = 167;
 
 
 static constexpr double EvalLevel[10] = {0.981, 0.956, 0.895, 0.949, 0.913,
