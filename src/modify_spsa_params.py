38f365514b46096768f21f680fb3c6bea1e8ac5f
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-04 13:15:37 -0400

    Try 32 for c
    bench 1209805

--- src/modify_spsa_params.py
+++ src/modify_spsa_params.py
@@ -15,12 +15,12 @@ with open(filename, "r") as f:
 
 # c value 4 for weights [-127, 127]
 # c value 128 for biases (-20000, 20000)
-# c value 16 for ft biases? (-2048, 2048)
+# c value 32 for ft biases? (-2048, 2048)
 print(f"Output params in: {output_filename}")
 with open(output_filename, "w") as f:
     for row in rows:
         if row[0].split("[")[0][-1] == "W":
             row[4] = "4"
-        elif row[0].split("[")[0][-1] == "B":   # try 16
-            row[4] = "16"
+        elif row[0].split("[")[0][-1] == "B":   # try 32
+            row[4] = "32"
         f.write(",".join(row) + "\n")
