ca4138055abf483c5cc9557c092ff7d576329ddd
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-12 12:25:46 -0400

    fv as bv
    bench 1386580

--- src/search.cpp
+++ src/search.cpp
@@ -1035,7 +1035,7 @@ moves_loop:  // When in check, search starts here
                 {
                     if (bestValue <= futilityValue && std::abs(bestValue) < VALUE_TB_WIN_IN_MAX_PLY
                         && futilityValue < VALUE_TB_WIN_IN_MAX_PLY)
-                        bestValue = (bestValue + futilityValue * 7) / 8;
+                        bestValue = futilityValue;
                     continue;
                 }
 
