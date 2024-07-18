c4b00f189d6476a579edc06c7e2a286f9fa92f30
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-17 22:26:48 -0400

    Add cache

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -62,8 +62,9 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     bool smallNet = use_smallnet(pos);
     int  v;
 
-    auto [psqt, positional] = smallNet ? networks.small.evaluate(pos, &caches.small)
-                                       : networks.big.evaluate(pos, &caches.big);
+    auto [psqt, positional] = smallNet         ? networks.small.evaluate(pos, &caches.small)
+                            : simpleEval > 700 ? networks.medium.evaluate(pos, &caches.medium)
+                                               : networks.big.evaluate(pos, &caches.big);
 
     Value nnue = (125 * psqt + 131 * positional) / 128;
 
