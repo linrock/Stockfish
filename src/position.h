f177ab47c7c20444f255eafa746f39ba3bbbfa4b
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-17 22:19:12 -0400

    Try adding a 3rd nnue

--- src/position.h
+++ src/position.h
@@ -60,9 +60,10 @@ struct StateInfo {
     int        repetition;
 
     // Used by NNUE
-    Eval::NNUE::Accumulator<Eval::NNUE::TransformedFeatureDimensionsBig>   accumulatorBig;
-    Eval::NNUE::Accumulator<Eval::NNUE::TransformedFeatureDimensionsSmall> accumulatorSmall;
-    DirtyPiece                                                             dirtyPiece;
+    Eval::NNUE::Accumulator<Eval::NNUE::TransformedFeatureDimensionsBig>    accumulatorBig;
+    Eval::NNUE::Accumulator<Eval::NNUE::TransformedFeatureDimensionsMedium> accumulatorMedium;
+    Eval::NNUE::Accumulator<Eval::NNUE::TransformedFeatureDimensionsSmall>  accumulatorSmall;
+    DirtyPiece                                                              dirtyPiece;
 };
 
 
