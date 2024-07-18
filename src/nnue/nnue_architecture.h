8009e30e6b8393a5ca319b1e8302e9395ba71a2f
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-17 22:19:12 -0400

    Try adding a 3rd nnue

--- src/nnue/nnue_architecture.h
+++ src/nnue/nnue_architecture.h
@@ -42,6 +42,10 @@ constexpr IndexType TransformedFeatureDimensionsBig = 3072;
 constexpr int       L2Big                           = 15;
 constexpr int       L3Big                           = 32;
 
+constexpr IndexType TransformedFeatureDimensionsMedium = 1024;
+constexpr int       L2Medium                           = 15;
+constexpr int       L3Medium                           = 32;
+
 constexpr IndexType TransformedFeatureDimensionsSmall = 128;
 constexpr int       L2Small                           = 15;
 constexpr int       L3Small                           = 32;
