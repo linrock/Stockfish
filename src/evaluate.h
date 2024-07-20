c4f1462155f6484de9f00eb695c25fe6df605b12
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-19 22:13:51 -0400

    900-975 700
    bench 1295260

--- src/evaluate.h
+++ src/evaluate.h
@@ -34,7 +34,7 @@ namespace Eval {
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
 #define EvalFileDefaultNameBig "nn-1111cefa1111.nnue"
-#define EvalFileDefaultNameMedium "nn-df727539572b.nnue"
+#define EvalFileDefaultNameMedium "nn-738662c8e8b7.nnue"
 #define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
 
 namespace NNUE {
