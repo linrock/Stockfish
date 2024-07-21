14459d8b751a446cedc3920997d6635a6ad56c3a
Linmiao Xu <linmiao.xu@gmail.com>
2024-07-21 10:05:19 -0400

    step func for midnet up to 96
    bench 1532705

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -47,11 +47,8 @@ int Eval::simple_eval(const Position& pos, Color c) {
 
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
-    if (pos.count<ALL_PIECES>() <= 8) {
-      return std::abs(simpleEval) > 962;
-    } else {
-      return std::abs(simpleEval) > 962 + (pos.count<ALL_PIECES>() - 8) * 4;
-    }
+    int pc = pos.count<ALL_PIECES>();
+    return std::abs(simpleEval) > 962 + (pc > 24) * 32 + (pc > 16) * 32 + (pc > 8) * 32;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
