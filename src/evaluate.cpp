d1f69a5db93995ca76048f2aed1051e622124628
Linmiao Xu <linmiao.xu@gmail.com>
2024-08-04 18:35:55 -0400

    100 100 100 50
    bench 1533686

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -71,7 +71,7 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     int pc = pos.count<ALL_PIECES>();
     if (smallNet
         && (nnue * psqt < 0
-            || std::abs(nnue) < 200 + 50 * (pc > 7) + 50 * (pc > 15) + 50 * (pc > 23)))
+            || std::abs(nnue) < 100 + 100 * (pc > 7) + 100 * (pc > 15) + 50 * (pc > 23)))
     {
         std::tie(psqt, positional) = networks.big.evaluate(pos, &caches.big);
         nnue                       = (125 * psqt + 131 * positional) / 128;
