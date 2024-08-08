c2435da9fb8c94606cce1b1fc11f4a8608866ef3
Linmiao Xu <linmiao.xu@gmail.com>
2024-08-07 21:15:51 -0400

    128--S1-simpleEval-gt925-no-wld-skip-sk28
    ep319
    
    bench 1252584

--- src/evaluate.h
+++ src/evaluate.h
@@ -34,7 +34,7 @@ namespace Eval {
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
 #define EvalFileDefaultNameBig "nn-31337bea577c.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameSmall "nn-5afe67ec7460.nnue"
 
 namespace NNUE {
 struct Networks;
