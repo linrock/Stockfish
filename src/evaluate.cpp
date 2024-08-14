8477a94bfdd9fcb42f47441a74903205e6302d97
Linmiao Xu <linmiao.xu@gmail.com>
2024-08-13 20:26:26 -0400

    with stronger main net
    bench 1675709

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -48,7 +48,7 @@ int Eval::simple_eval(const Position& pos, Color c) {
 bool Eval::use_smallnet(const Position& pos) {
     int simpleEval = simple_eval(pos, pos.side_to_move());
     int pc = pos.count<ALL_PIECES>();
-    return std::abs(simpleEval) > 960 + (pc > 24) * 16 + (pc > 16) * 16 + (pc > 8) * 16;
+    return std::abs(simpleEval) > 955 + (pc > 24) * 16 + (pc > 16) * 16 + (pc > 8) * 16;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
