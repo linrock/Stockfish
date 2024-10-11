c7acf93c0b0101ec0b47ada97a5b3f6bc02c3814
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-10 21:59:31 -0400

    Without sn
    bench 1444083

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,8 +46,9 @@ int Eval::simple_eval(const Position& pos, Color c) {
 }
 
 bool Eval::use_smallnet(const Position& pos) {
-    int simpleEval = simple_eval(pos, pos.side_to_move());
-    return std::abs(simpleEval) > 962;
+    return false;
+    // int simpleEval = simple_eval(pos, pos.side_to_move());
+    // return std::abs(simpleEval) > 962;
 }
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
