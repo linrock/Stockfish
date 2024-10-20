186da07577dfd301a72d38c6fc8b7a3b4411e83b
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-20 12:14:15 -0400

    noSn S1 ep399 3072--S1-UHOx3-oct2024-layerStacks16
    bench 1500453

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,8 +33,8 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-1111cefa1111.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameBig "nn-d0fe56407ee5.nnue"
+#define EvalFileDefaultNameSmall "nn-d261fddc6eca.nnue"
 
 namespace NNUE {
 struct Networks;
