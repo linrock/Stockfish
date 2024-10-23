5d9595b7a3c4df2f02106763f3dd54d703a8fec1
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-23 02:50:20 -0400

    2f6bd4: 648 oneB twoB oW oB 12k (190W 388B)
    bench 1472150

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultNameBig "nn-369e5b4ae293.nnue"
+#define EvalFileDefaultNameBig "nn-45d915c5dbc2.nnue"
 #define EvalFileDefaultNameSmall "nn-aa2736ae40b1.nnue"
 
 namespace NNUE {
