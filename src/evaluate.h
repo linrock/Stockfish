5fdb18a7e40496bf67998207c7877b2f72e7696b
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-25 18:59:13 -0400

    37f18f: 264 oW oB 16k (190W 7B)
    bench 1198776

--- src/evaluate.h
+++ src/evaluate.h
@@ -34,7 +34,7 @@ namespace Eval {
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
 #define EvalFileDefaultNameBig "nn-1111cefa1111.nnue"
-#define EvalFileDefaultNameSmall "nn-37f18f62d772.nnue"
+#define EvalFileDefaultNameSmall "nn-6c48cc1ed531.nnue"
 
 namespace NNUE {
 struct Networks;
