18dbe43172a389abab0a3f5cbbdd04e5e10c63c0
Linmiao Xu <linmiao.xu@gmail.com>
2024-10-25 18:22:37 -0400

    q16 over 512
    bench 1801142

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -81,12 +81,8 @@ Value Eval::evaluate(const Eval::NNUE::Networks&    networks,
     int material = (smallNet ? 553 : 532) * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * (77777 + material) + optimism * (7777 + material)) / 77777;
 
-    if (std::abs(v) > 1024)
-        v = (v / 32) * 32;
-    else if (std::abs(v) > 512)
+    if (std::abs(v) > 512)
         v = (v / 16) * 16;
-    else if (std::abs(v) > 256)
-        v = (v / 8) * 8;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 212;
