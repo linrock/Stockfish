b43906999c74a3f05910d27e324d6c717f84edc6
Linmiao Xu <linmiao.xu@gmail.com>
2024-11-14 12:13:26 -0500

    q255 S13 ep679 / 4 oWB 13k + 3rd sn
    bench 925547

--- src/nnue/nnue_feature_transformer.h
+++ src/nnue/nnue_feature_transformer.h
@@ -336,6 +336,7 @@ class FeatureTransformer {
           / 2;
 
         const auto& accumulation = (pos.state()->*accPtr).accumulation;
+        constexpr int QuantizedOne = TransformedFeatureDimensions == TransformedFeatureDimensionsBig ? 255 : 254;
 
         for (IndexType p = 0; p < 2; ++p)
         {
@@ -348,8 +349,7 @@ class FeatureTransformer {
             constexpr IndexType NumOutputChunks = HalfDimensions / 2 / OutputChunkSize;
 
             const vec_t Zero = vec_zero();
-            const vec_t One  = vec_set_16(
-              TransformedFeatureDimensions == TransformedFeatureDimensionsBig ? 255 : 254);
+            const vec_t One  = vec_set_16(QuantizedOne);
 
             const vec_t* in0 = reinterpret_cast<const vec_t*>(&(accumulation[perspectives[p]][0]));
             const vec_t* in1 =
@@ -438,8 +438,8 @@ class FeatureTransformer {
                 BiasType sum0 = accumulation[static_cast<int>(perspectives[p])][j + 0];
                 BiasType sum1 =
                   accumulation[static_cast<int>(perspectives[p])][j + HalfDimensions / 2];
-                sum0               = std::clamp<BiasType>(sum0, 0, TransformedFeatureDimensions == TransformedFeatureDimensionsBig ? 255 : 254);
-                sum1               = std::clamp<BiasType>(sum1, 0, TransformedFeatureDimensions == TransformedFeatureDimensionsBig ? 255 : 254);
+                sum0               = std::clamp<BiasType>(sum0, 0, QuantizedOne);
+                sum1               = std::clamp<BiasType>(sum1, 0, QuantizedOne);
                 output[offset + j] = static_cast<OutputType>(unsigned(sum0 * sum1) / 512);
             }
 
