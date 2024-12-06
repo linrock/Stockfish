f83b2c378f83d358e6adc5e92788c4242f6ee142
Linmiao Xu <linmiao.xu@gmail.com>
2024-12-06 12:34:03 -0800

    less lazy
    bench 3385351

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,8 +191,8 @@ using namespace Trace;
 namespace {
 
   // Threshold for lazy and space evaluation
-  constexpr Value LazyThreshold1    =  Value(3622);
-  constexpr Value LazyThreshold2    =  Value(1962);
+  constexpr Value LazyThreshold1    =  Value(3941);
+  constexpr Value LazyThreshold2    =  Value(2031);
   constexpr Value SpaceThreshold    =  Value(11551);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
