7199cd832d6f05fe8277961ff5c3e32f72db4395
Linmiao Xu <linmiao.xu@gmail.com>
2025-02-19 02:04:23 -0500

    HL96-hse-900-1k-sb50-50.bin
    bench 2236599

--- src/tiny_nnue.h
+++ src/tiny_nnue.h
@@ -3,10 +3,10 @@
 #include "simd.h"
 #include "types.h"
 
-#define TinyEvalFile "HL128-hse-900-1k-sb30-30.bin"
+#define TinyEvalFile "HL96-hse-900-1k-sb50-50.bin"
 
 // (768 -> HL)x2 -> 1
-#define HIDDEN_WIDTH   128
+#define HIDDEN_WIDTH    96
 
 #define NETWORK_SCALE  340
 #define NETWORK_QA     512
