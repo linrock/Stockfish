dae8c6b3af597da2a56b743ebb42e29c429afda8
Linmiao Xu <linmiao.xu@gmail.com>
2025-02-19 00:06:07 -0500

    HL64 hse 1k+
    bench 2207247

--- src/tiny_nnue.h
+++ src/tiny_nnue.h
@@ -3,10 +3,10 @@
 #include "simd.h"
 #include "types.h"
 
-#define TinyEvalFile "HL128-hse-1k-50.bin"
+#define TinyEvalFile "HL64-hse-1k-50.bin"
 
 // (768 -> HL)x2 -> 1
-#define HIDDEN_WIDTH   128
+#define HIDDEN_WIDTH    64
 
 #define NETWORK_SCALE  340
 #define NETWORK_QA     255
