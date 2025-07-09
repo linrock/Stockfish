841443453287bb0f8016f68ad6c0f4720f62d8bb
Linmiao Xu <linmiao.xu@gmail.com>
2025-07-09 22:12:32 +0300

    sn th 1012, 962
    bench 2193011

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -46,7 +46,10 @@ int Eval::simple_eval(const Position& pos) {
          + (pos.non_pawn_material(c) - pos.non_pawn_material(~c));
 }
 
-bool Eval::use_smallnet(const Position& pos) { return std::abs(simple_eval(pos)) > 1012; }
+bool Eval::use_smallnet(const Position& pos) {
+  const int th = pos.count<ALL_PIECES>() > 16 ? 1012 : 962;
+  return std::abs(simple_eval(pos)) > th;
+}
 
 // Evaluate is the evaluator for the outer world. It returns a static evaluation
 // of the position from the point of view of the side to move.
