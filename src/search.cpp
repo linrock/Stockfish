21bfcfa4155ce40f8141a3327f9cbc750525a1f8
Linmiao Xu <linmiao.xu@gmail.com>
2026-04-06 22:35:17 -0400

    less neg ext pv node
    bench 3010707

--- src/search.cpp
+++ src/search.cpp
@@ -1188,7 +1188,7 @@ moves_loop:  // When in check, search starts here
             // ttMove in favor of other moves based on some conditions:
 
             // If the ttMove is assumed to fail high over current beta
-            else if (ttData.value >= beta)
+            else if (!PvNode && ttData.value >= beta)
                 extension = -3;
 
             // If we are on a cutNode but the ttMove is not assumed to fail high
