66e687200642fb249df964c82b23daeab49ec066
Linmiao Xu <linmiao.xu@gmail.com>
2026-04-07 20:33:13 -0400

    simplify all node reduction
    bench 3080472

--- src/search.cpp
+++ src/search.cpp
@@ -1242,7 +1242,7 @@ moves_loop:  // When in check, search starts here
 
         // Scale up reductions for expected ALL nodes
         if (allNode)
-            r += r * 273 / (256 * depth + 260);
+            r += r / (depth + 1);
 
         // Step 17. Late moves reduction / extension (LMR)
         if (depth >= 2 && moveCount > 1)
